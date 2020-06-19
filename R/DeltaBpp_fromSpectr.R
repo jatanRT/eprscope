f4c9ecc1228a0545c83a01c69314b03d3f861599
JanT <jantar40@protonmail.com>
2020-06-19 23:01:30 +0200

    Function to calulate DeltaBpp updated

--- R/DeltaBpp_fromSpectr.R
+++ R/DeltaBpp_fromSpectr.R
@@ -1,20 +1,30 @@
 #
-#' @title Claculation of
+#' @title Calculation of Spectral Linewidth (\eqn{\Delta B_pp}) of the EPR Spectrum
 #'
 #'
-#' @description Blah
+#' @description The function calculates the \eqn{\Delta B_pp} (peak-to-peak) linewidth
+#'   of EPR spectrum. The difference corresponds to magnetic flux densities corresponding
+#'   to minimum and maximum of the \code{dIepr_over_dB} in the selected \code{B} region.
 #'
 #'
-#' @param spectrum.data
-#' @param B.reg.start
-#' @param B.reg.end
+#' @param spectrum.data Spectrum data frame/table where the magnetic flux density (in \code{mT}) column
+#'   must be labeled as \code{B_mT} and that of the derivative intensity as \code{dIepr_over_dB},
+#'   \code{index} column can be included as well
+#' @param B.reg.start Numeric, magnetic flux density in \code{mT} corresponding to \code{starting} border
+#'   of the \code{selected B region} (therefore abbreviation \code{.reg.})
+#' @param B.reg.end Numeric, magnetic flux density in \code{mT} corresponding to \code{ending} border
+#'   of the \code{selected B region} (therefore abbreviation \code{.reg.})
 #'
 #'
-#' @return
+#' @return Difference (the absolute value) of the magnetic flux densities (\code{B}) corresponding to \code{minimum}
+#'   and \code{maximum} of the derivative intensity (\code{dIepr_over_dB}) => \eqn{\Delta B_pp}
 #'
 #'
 #' @examples
-#'
+#' \dontrun{
+#' DeltaBpp_fromSpectr(spectrum.data,320.221,328.331)
+#' DeltaBpp_fromSpectr(spectrum.data,B.reg.start = 320.221,B.reg.end = 328.331)
+#' }
 #'
 #' @export
 #'
