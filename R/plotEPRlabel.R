708eefd3da98702b48307969a14febe978cc036e
JanT <jantar40@protonmail.com>
2020-06-23 16:18:54 +0200

    Updated label funct. after complete rewrt.

--- R/plotEPRlabel.R
+++ R/plotEPRlabel.R
@@ -1,19 +1,31 @@
 #
-#' @title
+#' @title Labels for Various EPR Spectroscopy Plots
 #'
 #'
-#' @description
+#' @description TODO To write greek symbols, subscript and superscript, see
+#'   \href{https://www.r-bloggers.com/math-notation-for-r-plot-titles-expression-and-bquote/}{R-Bloggers:Math Notation}
+#'   and \href{https://www.r-bloggers.com/r-plotmath-functions-combined-with-variable-values/}{R-Bloggers:Plotmath}
+#'   or \code{\link[grDevices]{plotmath}} (in console write \code{?plotmath})
 #'
 #'
-#' @param quantity
-#' @param unit
-#' @param user.defined
+#' @param quantity Variable String (without quotation, sometimes part of it can be quoted, see examples below),
+#'   physical quantity, which should be displayed as the axis title like \eqn{B},
+#'   d\eqn{I}_EPR/d\eqn{B}, \eqn{time}, \eqn{\Delta B_pp}, \code{Double Integral} etc.
+#' @param unit Variable String (without quotation, sometimes part of it can be quoted, see examples below),
+#'   physical quanity corresponding unit, which should be displayed like \code{mT}, \code{s}, \code{p.d.u.}, etc.
+#' @param user.defined Boolean, in order to bring more flexibility to display more complicated quantities and units
+#'   based on users requirements (sse examples bellow)
 #'
 #'
-#' @return
+#' @return TODO
 #'
 #'
 #' @examples
+#' plotEPRlabel(B,mT)
+#' plotEPRlabel("d"~italic(I)[EPR]~"/"~"d"~italic(B),p.d.u.,user.defined = T)
+#' plotEPRlabel(quantity = Delta*B[pp],unit = mT,user.defined = F)
+#' plotEPRlabel(E,"("~V~")"~~~italic(vs)~~~italic(Ref.~Electrode),user.defined = T)
+#' plotEPRlabel(c,mmol*dm^-3)
 #'
 #'
 #' @export
@@ -28,7 +40,7 @@ plotEPRlabel <- function(quantity,unit,user.defined = F){
   if (isFALSE(user.defined)){
     l <- bquote(italic(.(quantity))~~"("~.(unit)~")")
   } else{
-    l <- bquote(italic(.(quantity))~~.(unit))
+    l <- bquote(.(quantity)~~.(unit))
   }
   return(l)
 }
