e23b3126bb07b46851f40296a42a1a9997c33bf7
JanT <jantar40@protonmail.com>
2021-03-12 14:37:06 +0100

    added `B` option (incl. 'B_G') in `Bextrem...`

--- R/Bextrem_fromSpectr.R
+++ R/Bextrem_fromSpectr.R
@@ -4,6 +4,7 @@
 #' @description TODO
 #'
 #' @param spectrum.data TODO
+#' @param B TODO
 #' @param B.reg.start TODO
 #' @param B.reg.end TODO
 #' @param extrem TODO
@@ -18,23 +19,23 @@
 #' @export
 #'
 ## COMMENT Find B-extremes (magnetic flux density) in EPR spectra:
-## Function to find B ('B_mT') corresponding to max. or min.
+## Function to find B ('B_mT' or 'B_G') corresponding to max. or min.
 ## intensity ('dIepr_over_dB'). To select a region / span / interval
 ## (limits are 'B.reg.start','B.reg.end')
 ## of single spectrum in which the extrem ("max" or "min") has to be found
-Bextrem_fromSpectr  <-  function(spectrum.data,B.reg.start,B.reg.end,extrem){
+Bextrem_fromSpectr  <-  function(spectrum.data,B="B_mT",B.reg.start,B.reg.end,extrem){
   if (extrem == "min"){
     B.min  <-  spectrum.data  %>%
-      filter(between(.data$B_mT,B.reg.start,B.reg.end))  %>%
+      filter(between(.data[[B]],B.reg.start,B.reg.end))  %>%
       filter(.data$dIepr_over_dB == min(.data$dIepr_over_dB))  %>%
-      pull(.data$B_mT)
+      pull(.data[[B]])
     return(B.min)
   }
   if (extrem == "max"){
     B.max  <-  spectrum.data  %>%
-      filter(between(.data$B_mT,B.reg.start,B.reg.end))  %>%
+      filter(between(.data[[B]],B.reg.start,B.reg.end))  %>%
       filter(.data$dIepr_over_dB == max(.data$dIepr_over_dB))  %>%
-      pull(.data$B_mT)
+      pull(.data[[B]])
     return(B.max)
   }
 }
