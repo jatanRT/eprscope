60e40e35cd6814e4ecb3e5cf6dc3ff01bcc47bdd
JanT <jantar40@protonmail.com>
2021-03-15 14:51:18 +0100

    `...NoY...` theme updated (added params.)

--- R/plot_theme_NoY_ticks.R
+++ R/plot_theme_NoY_ticks.R
@@ -5,9 +5,10 @@
 #' @description TODO
 #'
 #'
-#' @param base.theme TODO
+#' @param basic.theme TODO
 #' @param axis.text.size TODO
 #' @param axis.title.size TODO
+#' @param grid TODO
 #' @param bg.transparent TODO
 #'
 #'
@@ -23,21 +24,39 @@
 #'
 #'
 #' @importFrom ggplot2 %+replace%
-plot_theme_NoY_ticks <- function(base.theme = theme_gray(),axis.text.size,axis.title.size,bg.transparent = FALSE){
-  ## theme part
-  theme_01 <- theme(axis.ticks.length = unit(-6,"pt"),
+plot_theme_NoY_ticks <- function(basic.theme = theme_gray(),
+                                 axis.text.size = 15,
+                                 axis.title.size = 17,
+                                 grid = TRUE,
+                                 bg.transparent = FALSE){
+  ## theme parts:
+  theme_bas <- theme(axis.ticks.length = unit(-6,"pt"),
         axis.text.x = element_text(margin = margin(10,8,6,8,unit = "pt"),size = axis.text.size),
         axis.text.y = element_blank(), axis.ticks.y = element_blank(),
         axis.title.y = element_text(margin = margin(2,12,2,6,unit = "pt"),size = axis.title.size),
         axis.title.x = element_text(margin = margin(2,6,2,6,unit = "pt"),size = axis.title.size),
         panel.border = element_rect(color = "black",fill = NA))
+  theme_Nogrid <- theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank())
   ## theme:
   if (isTRUE(bg.transparent)){
-    base.theme %+replace%
-      theme_01 +
-      theme(plot.background = element_rect(fill = "transparent"))
+    if (isTRUE(grid)){
+      base.theme %+replace%
+        theme_bas +
+        theme(plot.background = element_rect(fill = "transparent"))
+    } else{
+      base.theme %+replace%
+        theme_bas +
+        theme_Nogrid +
+        theme(plot.background = element_rect(fill = "transparent"))
+    }
   } else{
-    base.theme %+replace%
-      theme_01
+    if (isTRUE(grid)){
+      base.theme %+replace%
+        theme_bas
+    } else{
+      base.theme %+replace%
+        theme_bas +
+        theme_Nogrid
+    }
   }
 }
