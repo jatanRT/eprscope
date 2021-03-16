519280e7b76b906dfe165bec87f6280e972bcaa4
JanT <jantar40@protonmail.com>
2021-03-16 10:31:29 +0100

    updated interact. spectral plot(several ones+time)

--- R/plotEPRspectr_interactive.R
+++ R/plotEPRspectr_interactive.R
@@ -12,24 +12,32 @@
 #' @param B Character/String pointing to magnetic flux density \code{column} of EPR spectrum data frame
 #'   \code{spectrum.data} either in \code{millitesla} or in \code{Gauss}, that is \code{B = "B_mT"} (default)
 #'   or \code{B = "B_G"}
-#' @param line.color String, line color to plot simple EPR spectrum. All \pkg{ggplot2} compatible
-#'   colors are allowed, default: \code{line.color = "steelblue"}
 #' @param line.size Numeric, linewidth of the plot line in \code{pt}, default: \code{line.size = 0.75}
+#' @param time.series Boolean, whether the input ASCII spectrum data comes from the time series experiment
+#'   with the additional \code{time} (labeled as \code{time_s}) column (ONLY IN CASE of "Xenon" software)
+#'
 #'
 #' @return TODO
+#'
 #'
 #' @examples
 #' TODO
 #' TODO
 #'
+#'
 #' @export
 #'
 #'
 #' @importFrom plotly ggplotly
-plotEPRspectr_interactive <- function(spectrum.data,B = "B_mT",line.color = "steelblue",line.size = 0.75){
+plotEPRspectr_interactive <- function(spectrum.data,B = "B_mT",line.size = 0.75,time.series = FALSE){
   ## plot precursor
-  simplePlot <- ggplot(spectrum.data,aes(x = .data[[B]], y = .data$dIepr_over_dB)) +
-    geom_line(size = line.size,color = line.color,show.legend = FALSE)
+  if (isTRUE(time.series)){
+    simplePlot <- ggplot(spectrum.data,aes(x = .data[[B]], y = .data$dIepr_over_dB,color = .data$times_s)) +
+      geom_line(size = line.size,show.legend = TRUE)
+  } else{
+    simplePlot <- ggplot(spectrum.data,aes(x = .data[[B]], y = .data$dIepr_over_dB)) +
+      geom_line(size = line.size,show.legend = FALSE)
+  }
   ##
   return(ggplotly(simplePlot))
 }
