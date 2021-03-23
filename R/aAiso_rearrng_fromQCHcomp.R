e0b76e056e71ea29c220ce4c0997b8bc4ac933be
JanT <jantar40@protonmail.com>
2021-03-23 13:00:44 +0100

    added comment to newly added function:-)

--- R/aAiso_rearrng_fromQCHcomp.R
+++ R/aAiso_rearrng_fromQCHcomp.R
@@ -1,6 +1,6 @@
 #
 aAiso_rearrng_fromQCHcomp <- function(path_to_ASC,data.col.names,nuclei.list.slct){
 
-  ## use stringr::str_subset(...) or X[grepl(...)]
+  ## use stringr::str_subset(...) or X[grepl(...)] or stringr::str_extract
 
 }
