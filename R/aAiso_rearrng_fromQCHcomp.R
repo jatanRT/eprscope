ce19e0c9ae14a84fff9ef0afa6e23222f84e5bd4
JanT <jantar40@protonmail.com>
2021-03-23 12:54:57 +0100

    created new funct. (AaQCH) + docu (`gVal..`) updated

---
+++ R/aAiso_rearrng_fromQCHcomp.R
@@ -0,0 +1,6 @@
+#
+aAiso_rearrng_fromQCHcomp <- function(path_to_ASC,data.col.names,nuclei.list.slct){
+
+  ## use stringr::str_subset(...) or X[grepl(...)]
+
+}
--- R/gValue_fromQCHcomp.R
+++ R/gValue_fromQCHcomp.R
@@ -5,7 +5,7 @@
 #' @description TODO
 #'
 #' @param delta_g_vec Numeric vector of principal \code{g-value differences in 'ppm'}
-#'   from \eqn{g_e} within e.g. Gaussian output
+#'   from \eqn{g_e} within e.g. \strong{Gaussian} or \strong{ORCA} output
 #' @param mean Boolean, whether to calculated the \code{mean value} of \code{delta_g_vec} elements,
 #'   \strong{default}: \code{mean = TRUE}, or save the entire vector with all principal components
 #'
