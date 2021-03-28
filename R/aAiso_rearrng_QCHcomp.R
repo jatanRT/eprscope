bad6c7ead9a8365f3782ec3f2141828a3b314f5b
JanT <jantar40@protonmail.com>
2021-03-28 22:39:45 +0200

    last 2 new functions updated

--- R/aAiso_rearrng_QCHcomp.R
+++ R/aAiso_rearrng_QCHcomp.R
@@ -23,45 +23,58 @@
 #'
 #' @importFrom stringr str_subset str_extract
 aAiso_rearrng_QCHcomp <- function(path_to_ASC,data.col.names,nuclei.list.slct){
+  #
   ## Conditions/Extraction for column names:
   ## use stringr::str_subset(...) or X[grepl(...)] or stringr::str_extract or grep(...,value = T)
   A.str <- str_subset(data.col.names,regex("mhz",ignore_case = T))
   a.str <- str_subset(data.col.names,regex("gauss|G|Gauss"))
   atomic.num.str <- str_subset(data.col.names,regex("No|Num|num|no"))
   nucl.str <- str_subset(data.col.names,regex("nuc",ignore_case = T))
+  #
   ## Read the data:
   data.Aa.comput <- data.table::fread(path_to_ASC,sep = "auto",header = F,skip = 1,
                                       col.names = data.col.names) %>%
     mutate(MegaHertz = abs(round(.data[[A.str]],digits = 3)),
            Gauss = abs(round(.data[[a.str]],digits = 2))) %>%
     select(c(.data[[atomic.num.str]],.data[[nucl.str]],.data$MegaHertz,.data$Gauss))
+  #
   ## Own function to rearrange A/a according to `nuclei.list.slct`
   ## Build up new rearranged data frame for Nuclei A/a:
   data.slct.nucs.group <- data.frame(No = integer(),Nucleus = character(),
                               MegaHertz = double(),Gauss = double(),NuclearGroup = character())
+  #
   ## cycle for each `nuclei.list.slct` component
   for (k in 1:length(nuclei.list.slct)){
+    #
     ## Filter atomic numbers from each part of the list:
     sal <- data.Aa.comput %>%
       filter(.data[[atomic.num.str]] %in% nuclei.list.slct[[k]])
+    #
     ## How many atoms/nuclei are included in each list part:
     how.many.nucs <- length(nuclei.list.slct[[k]])
+    #
     ## Extract atomic/nuclear label:
     mark.nucs <- str_extract(sal[[nucl.str]][1],"[[:alpha:]]+")
+    #
     ## Extract isotope number:
     nucleo.nucs <- str_extract(sal[[nucl.str]][1],"[[:digit:]]+")
+    #
     ## Extract atomic/nuclear numbers and collapse it into one string:
     num.nucs.str <- paste(sal[[atomic.num.str]],collapse = ",")
+    #
     ## Now combine all four variables by paste:
     group.nucs <- paste(how.many.nucs," x ",nucleo.nucs,mark.nucs," (",num.nucs.str,")",sep = "")
+    #
     ## Replicate the previous string variable corresponding to length of a list component/part:
     nuclear.group <- rep(group.nucs,length(nuclei.list.slct[[k]]))
+    #
     ## Build up data frame row by row:
     gal <- data.frame(sal,nuclear.group) ## at first combine by columns
     data.slct.nucs.group <- rbind(data.slct.nucs.group,gal)
   }
   ## Group by and summarize (find the mean value of each list component) according
   ## to `nuclei.list.slct` (symmetry) and previous `for loop`:
+  #
   data.slct.nucs.group <- data.slct.nucs.group %>%
     mutate(mT = .data$Gauss/10) %>%
     select(-c(.data$No,.data$Nucleus,.data$Gauss)) %>%
--- R/integ_correct_EPRspecs.R
+++ R/integ_correct_EPRspecs.R
@@ -1,4 +1,35 @@
 #
+#' @title Polynomial Baseline Correction for single-Integrated EPR Spectra + Double Integral Calculation/Presentation
+#'
+#'
+#' @description TODO
+#'
+#'
+#' @param spec.integ.data Spectrum data frame/table with magnetic flux density (in \code{mT} or \code{G}) column
+#'   must be labeled as \code{B_mT} (or \code{B_G}) and that of the derivative intensity as \code{dIepr_over_dB},
+#'   \code{index} column may be included as well, \code{sinlge integrated EPR} \strong{spectrum must be included}
+#'   in column named by \code{"single|sinteg|s_integ|single_|singleinteg|sintegral|sInteg_"} ("|" == "or" operator),
+#'   this can be obtained by \code{\link[pracma:trapz]{pracma::cumtrapz}} function from \emph{B} and \emph{dIepr_over_dB}
+#' @param B Character/String pointing to magnetic flux density \code{column} of EPR spectrum data frame
+#'   \code{spectrum.data} either in \code{millitesla} or in \code{Gauss}, that is \code{B = "B_mT"}
+#'   or \code{B = "B_G"} (\strong{default})
+#' @param B.reg.start TODO
+#' @param B.reg.end TODO
+#' @param B.peak.start TODO
+#' @param B.peak.end TODO
+#' @param poly.degree TODO
+#' @param double.integ TODO
+#'
+#' @return data frame TODO
+#'
+#'
+#'
+#' @examples
+#'
+#'
+#' @export
+#'
+#'
 integ_correct_EPRspecs <- function(spec.integ.data,
                                    B = "B_G",
                                    B.reg.start,
@@ -6,34 +37,48 @@ integ_correct_EPRspecs <- function(spec.integ.data,
                                    B.peak.start,
                                    B.peak.end,
                                    poly.degree,
-                                   double.integ.val = FALSE){
+                                   double.integ = FALSE){
+  #
   ## Intensity column from spe.integ.data
   integ.string <- str_subset(colnames(spec.integ.data),
                              regex("single|sinteg|s_integ|single_|singleinteg|sintegral|sInteg_",
                                    ignore_case = T))
+  #
   ## select a region / range / interval of a integrated spectrum
   ## in which the second integral will be performed
   ## (limits are 'B.reg.start','B.reg.end'):
   data.slct <- spec.integ.data %>%
     filter(between(.data[[B]],B.reg.start,B.reg.end))
+  #
   ## select region / range / interval of the peak, which will be not
   ## considered ("!") for the baseline correction / fit
   ## (limits are 'B.peak.start','B.peak.end'):
   data.NoPeak <- data.slct %>%
     filter(!between(.data[[B]],B.peak.start,B.peak.end))
+  #
   ## Polynomial baseline fit:
   integ.baseline.fit <- lm(.data[[integ.string]] ~ stats::poly(.data[[B]],degree = poly.degree),
                            data = data.NoPeak)
+  #
   ## apply fit to data.slct, remove the .resid colum (which is not required),
   ## rename column with fit, subtract the baseline,
-  ## and finally shift the integral base up having all the values > 0 (subtract its minimum)
+  ## then shift the integral baseline up having all the values > 0 (subtract its minimum)
+  ## and finally calculate the double integral
   data.slct <- broom::augment(integ.baseline.fit,newdata = data.slct) %>%
     dplyr::select(-.data[[".resid"]]) %>%
     dplyr::rename(sIntegBaseLinFit = .data[[".fitted"]]) %>%
     dplyr::mutate(sIntegCorr = .data[[integ.string]] - .data$sIntegBaseLinFit) %>%
     dplyr::select(-sIntegBaselinFit) %>%
     dplyr::mutate(sIntegCorr - min(.data$sIntegCorr))
-
-
-
+  #
+  ## double integral calculation:
+  if (isTRUE(double.integ)){
+    result.integ.data <- data.slct %>%
+      dplyr::mutate(dIntegCorr = pracma::cumtrapz(.data[[B]],sIntegCorr))
+  } else{
+    result.integ.data <- data.slct
+  }
+  #
+  return(result.integ.data)
+  #
 }
