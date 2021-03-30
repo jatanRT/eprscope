d8a7eba0a9114615bb77fd1407e999408aa91dd3
JanT <jantar40@protonmail.com>
2021-03-30 09:38:25 +0200

    changed field variable in `readMAT...`

--- R/readMATfile_params.R
+++ R/readMATfile_params.R
@@ -8,7 +8,7 @@
 #' @param path_to_MAT String, path to \code{.mat} MATLAB file with all variables saved in workspace
 #' @param str.var String, \code{structure/variable}, which may contain \code{fields}, e.g. like \code{\strong{Sys}}
 #'   and \code{g} => \code{\strong{Sys}.g}, respectively
-#' @param var.field String, \code{field variable after 'dot'}, which is available only for certain
+#' @param field.var String, \code{field variable after 'dot'}, which is available only for certain
 #'   structures/variables, see e.g. example above (\code{Sys.\strong{g}}), therefore
 #'   the \strong{default} 'value' is \code{NULL} and \code{string} \strong{is applied only for structures with fields}.
 #'
@@ -26,13 +26,13 @@
 #'
 readMATfile_params <- function(path_to_MAT,
                                str.var,
-                               var.field = NULL){
+                               field.var = NULL){
   data.params <- R.matlab::readMat(path_to_MAT)
   #
-  if (is.null(var.field)){
+  if (is.null(field.var)){
     params <- data.params[[str.var]]
   } else{
-    params <- data.params[[str.var]][, ,1][[var.field]]
+    params <- data.params[[str.var]][, ,1][[field.var]]
   }
   #
   return(params)
