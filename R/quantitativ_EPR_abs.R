6b6316de48e5322fd947005e4a9fb45ea22699d0
JanT <jantar40@protonmail.com>
2021-03-31 23:16:23 +0200

    function fro quantitative abs updated

--- R/quantitativ_EPR_abs.R
+++ R/quantitativ_EPR_abs.R
@@ -29,7 +29,7 @@ quantitativ_EPR_abs <- function(double.integ,
   #
   if (microW.cavity == "rectangular"){
     #
-    ## Cavity constants/charracteristics:
+    ## Cavity constants/characteristics:
     point.sample.c.factor <- 8.51e-09 # unitless
     h.cavity.center <- 61 # in mm
     h.cavity.length <- 23 # in mm
@@ -56,9 +56,26 @@ quantitativ_EPR_abs <- function(double.integ,
       No.paramag.c <- (No.paramag.spc/Avogadro.No)/(tube.volume.m3/1e3)
       ## Result:
       Result <- list(N_cm = No.paramag.cm.spc,c_N_mol_dm3 = No.paramag.c)
-
     }
-
+    if (tube.h.mm >= h.cavity.length){
+      #
+      ## Integration of the polynomial function
+      integral.poly.list <- stats::integrate(intensity.poly.function,
+                                             lower = (h.cavity.center - h.cavity.length/2)*1e-3,
+                                             upper = (h.cavity.center + h.cavity.length/2)*1e-3)
+      integral.poly <- integral.poly.list[[1]]
+      #
+      ## Own quantification:
+      ## Number of species:
+      No.paramag.spc <- double.integ/((point.sample.c.factor/integral.poly)*Norm.const*third.quant.factor)
+      ## Number of species per effective cm
+      No.paramag.cm.spc <- No.paramag.spc/h.cavity.length/10
+      ## Number od species => concentration mol*dm^{-3}
+      No.paramag.c <- (No.paramag.spc/Avogadro.No)/((h.cavity.length*1e-3)*pi*((tube.id.mm/2)*1e-3)^2/1e3)
+      ## Result:
+      Result <- list(N_cm = No.paramag.cm.spc,c_N_mol_dm3 = No.paramag.c)
+    }
   }
+
 
 }
