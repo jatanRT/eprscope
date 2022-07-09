3bcca87f2da815edb67e2866b75535288e65a3a7
Jan T <jantar@protonmail.com>
2022-07-09 18:11:01 +0200

    find intensity extremes (`B` pos.) in spec. update

--- R/Bextreme_Spec.R
+++ R/Bextreme_Spec.R
@@ -1,60 +1,63 @@
 #
-#' @title Find Extreme (\strong{Min}imum or \strong{Max}imum) within the EPR Spectrum (or its Integrated Form)
+#' Find Intensity Extremes within the EPR Spectrum (or its Integrated Form).
 #'
-#' @description TODO
 #'
-#' @param spectrum.data Spectrum data frame/table with magnetic flux density (in \code{mT} or \code{G}) column
-#'   must be labeled as \code{B_mT} (or \code{B_G}) and that of the derivative intensity as \code{dIepr_over_dB},
-#'   \code{index} column may be included as well, integrated/simulated spectra (incl. other \code{Intensity}
-#'   and \code{B} columns) can be read as well
+#' @description Finding positions (\emph{B} in \code{mT} or \code{G}) of intensity minimum and/or maximum
+#' within selected region of the EPR spectrum
+#'
+#'
+#' @param spectrum.data EPR spectrum data frame/table with magnetic flux density (in \code{mT} or \code{G}) column
+#'   and that of the derivative \code{dIepr_over_dB} or integrated e.g. \code{Intensity}.
+#'   \code{Index} column may be included as well
 #' @param B Character/String pointing to magnetic flux density \code{column} of EPR spectrum data frame
 #'   \code{spectrum.data} either in \code{millitesla} or in \code{Gauss}, that is \code{B = "B_mT"} (\strong{default})
 #'   or \code{B = "B_G"} or \code{B = "B_G_Sim"} to include simulated EPR spectra as well
 #' @param Intensity Character/String pointing to \code{intensity column} if other than \code{dIepr_over_dB}
 #'   name/label is used (e.g. for simulated spectra), \strong{default}: \code{Intesity = "dIepr_over_dB"}
-#' @param B.reg.start Numeric, magnetic flux density in \code{mT} corresponding to \code{starting} border
-#'   of the \code{selected B region} (therefore abbreviation \code{.reg.})
-#' @param B.reg.end Numeric, magnetic flux density in \code{mT} corresponding to \code{ending} border
-#'   of the \code{selected B region} (therefore abbreviation \code{.reg.})
+#' @param Blim Numeric vector, magnetic flux density in \code{mT}/\code{G} corresponding to border limits
+#'   of the selected \emph{B} region, e.g. like `Blim = c(3495.4,3595.4)`
 #' @param extreme Character/String with only a two values allowed: \code{"min"} or \code{"max"}
 #'
-#' @return Numeric value of magnetic flux density \eqn{B} corresponding to \code{minimal} or \code{maximal}
+#' @returns Numeric value of magnetic flux density \emph{B} corresponding to \code{minimal} or \code{maximal}
 #'   \code{intensity} within the EPR spectra (or their integrated form)
 #'
+#'
 #' @examples
-#' TODO
-#' TODO
+#' \dontrun{
+#' Bextreme_Spec(spectrum.data,Blim = c(349.54,359.54),extreme = 'maximum')
+#' Bextreme_Spec(data.frame,"B_G",Intensity = "Integrated_Intensity",c(3495.4,3595.4),'minimum')
+#' }
 #'
 #'
 #' @export
 #'
+#'
 ## COMMENT Find B-extremes (magnetic flux density) in EPR spectra:
 ## Function to find B ('B_mT' or 'B_G') corresponding to max. or min.
 ## intensity ('dIepr_over_dB' or other name/label). To select a region / span / interval
-## (limits are 'B.reg.start','B.reg.end')
-## of single spectrum in which the extrem ("max" or "min") has to be found
+## (limits are `Blim`, `Blim[1]` = 'start',`Blim[2]` = 'end')
+## of single spectrum in which the extreme ("max" or "min") needs to be found
 Bextreme_Spec  <-  function(spectrum.data,
-                                   B = "B_mT",
-                                   Intensity = "dIepr_over_dB",
-                                   B.reg.start,
-                                   B.reg.end,
-                                   extreme){
+                            B = "B_mT",
+                            Intensity = "dIepr_over_dB",
+                            Blim,
+                            extreme){
   if (extreme == "min"){
     B.min  <-  spectrum.data  %>%
-      filter(between(.data[[B]],B.reg.start,B.reg.end))  %>%
+      filter(between(.data[[B]],Blim[1],Blim[2]))  %>%
       filter(.data[[Intensity]] == min(.data[[Intensity]]))  %>%
       pull(.data[[B]])
     #
-    return(B.min)
+    return(round(B.min,digits = 2))
     #
   }
   if (extreme == "max"){
     B.max  <-  spectrum.data  %>%
-      filter(between(.data[[B]],B.reg.start,B.reg.end))  %>%
+      filter(between(.data[[B]],Blim[1],Blim[2]))  %>%
       filter(.data[[Intensity]] == max(.data[[Intensity]]))  %>%
       pull(.data[[B]])
     #
-    return(B.max)
+    return(round(B.max,digits = 2))
     #
   }
 }
