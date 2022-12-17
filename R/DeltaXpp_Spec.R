656ce614d8a28dcc8da98b8c6b6167a076a765aa
Jan T <janinoterko@JTsComp.local>
2022-12-17 20:10:00 +0100

    implement. of more general quantity for `x`-axis

---
+++ R/DeltaXpp_Spec.R
@@ -0,0 +1,62 @@
+#
+#'
+#' Calculation of Spectral Linewidth ( e.g. \eqn{\Delta B_{pp}}) of the EPR/ENDOR Spectrum
+#'
+#'
+#' @description
+#' The function calculates the \code{peak-to-peak} (distance between of the \eqn{x}-axis
+#' projection of "min" and "max" derivative intensities) linewidth
+#' of an EPR/ENDOR spectrum.
+#'
+#'
+#' @param data.spectrum EPR/ENDOR spectrum data frame/table with magnetic flux density \eqn{B} (in \code{mT} or \code{G})
+#'   or \eqn{g}-Value or \eqn{RF} (in \code{MHz}) column and that of the derivative \code{dIepr_over_dB}
+#'   or integrated \code{Intensity}. \code{Index} column may be included as well.
+#' @param x Character/String pointing to \code{x}-axis/column quantity like magnetic flux density \eqn{B}, \eqn{g}-Value
+#'   or \eqn{RF} (radio frequency), \strong{default}: \code{x = "B_mT"}
+#' @param Intensity CCharacter/String pointing to \code{intensity column} if other than \code{dIepr_over_dB}
+#'   name/label is used (e.g. for simulated spectra), \strong{default}: \code{Intesity = "dIepr_over_dB"}
+#' @param xlim Numeric vector corresponding to border limits of the selected \eqn{x} region,
+#'   e.g. like `xlim = c(3495.4,3595.4)` (\eqn{B} in \code{G}) or `xlim = c(12.5,21.2)` (\eqn{RF} in \code{MHz})
+#'   or `xlim = c(2.004,2.001)` (\eqn{g} dimensionless)
+#'
+#'
+#' @return Numeric value of difference of \code{x}-axis quantity like \eqn{B},\eqn{g},\eqn{RF} (the absolute value)
+#'   corresponding to \code{minimum} and \code{maximum} of the derivative intensity (\code{dIepr_over_dB})
+#'   in EPR/ENDOR spectrum
+#'
+#'
+#' @examples
+#' \dontrun{
+#' DeltaBpp_Spec(data.spectrum,c(320.221,328.331))
+#' DeltaBpp_Spec(data.spectrum,B = "B_G",Intensity = "dIepr_over_dB",c(3202.11,3283.31))
+#' DeltaBpp_Spec(data.spectrum,"RF_MHz",xlim = c(10,42))
+#' DeltaBpp_Spec(data.spectrum,"B_mT_Sim",c(320.221,328.331))
+#' }
+#'
+#' @export
+#'
+#'
+DeltaXpp_Spec <- function(data.spectrum,
+                          x = "B_mT",
+                          Intensity = "dIepr_over_dB",
+                          xlim){
+  #
+  ## x corresponding to minimum and maximum derivative intensities
+  ## in the selected x region:
+  x.min <- data.spectrum %>%
+    filter(between(.data[[x]],xlim[1],xlim[2])) %>%
+    filter(.data[[Intensity]] == min(.data[[Intensity]])) %>%
+    pull(.data[[x]])
+  #
+  x.max <- data.spectrum %>%
+    filter(between(.data[[x]],xlim[1],xlim[2])) %>%
+    filter(.data[[Intensity]] == max(.data[[Intensity]])) %>%
+    pull(.data[[x]])
+  #
+  ## Delta_B calculation:
+  DeltaX_pp <- abs(x.min - x.max)
+  #
+  return(round(DeltaX_pp,digits = 2))
+  #
+}
---
+++ R/Xextreme_Spec.R
@@ -0,0 +1,63 @@
+#
+#' Find Intensity Extremes within the EPR/ENDOR Spectrum (incl. Integrated Form).
+#'
+#'
+#' @description Finding \code{x} positions like \eqn{B} ( in \code{mT} or \code{G}) or \eqn{g}
+#' or \eqn{RF} (in \code{MHz}) of intensity minimum and/or maximum within selected region of the EPR/ENDOR spectrum
+#'
+#'
+#' @param data.spectrum EPR/ENDOR spectrum data frame/table with magnetic flux density \eqn{B} (in \code{mT} or \code{G})
+#'   or \eqn{g}-Value or \eqn{RF} (in \code{MHz}) column and that of the derivative \code{dIepr_over_dB}
+#'   or integrated \code{Intensity}. \code{Index} column may be included as well.
+#' @param x Character/String pointing to \code{x}-axis/column quantity like magnetic flux density \eqn{B}, \eqn{g}-Value
+#'   or \eqn{RF} (radio frequency), \strong{default}: \code{x = "B_mT"}
+#' @param Intensity Character/String pointing to \code{intensity column} if other than \code{dIepr_over_dB}
+#'   name/label is used (e.g. for simulated spectra), \strong{default}: \code{Intesity = "dIepr_over_dB"}
+#' @param xlim Numeric vector corresponding to border limits of the selected \eqn{x} region,
+#'   e.g. like `xlim = c(3495.4,3595.4)` (\eqn{B} in \code{G}) or `xlim = c(12.5,21.2)` (\eqn{RF} in \code{MHz})
+#'   or `xlim = c(2.004,2.001)` (\eqn{g} dimensionless)
+#' @param extreme Character/String with only a two values allowed: \code{"min"} or \code{"max"}
+#'
+#' @returns Numeric value of \code{x}-axis quantity like \eqn{B},\eqn{g},\eqn{RF} corresponding
+#'  to \code{minimal} or \code{maximal} \code{intensity} within the EPR/ENDOR spectrum (or its integrated form)
+#'
+#'
+#' @examples
+#' \dontrun{
+#' Bextreme_Spec(data.spectrum,xlim = c(349.54,359.54),extreme = 'min')
+#' Bextreme_Spec(data.frame,"g_Value",Intensity = "Integrated_Intensity",c(2.007,2.000),'max')
+#' }
+#'
+#'
+#' @export
+#'
+#'
+## COMMENT Find extremes in EPR spectra:
+## Function to find B,g or RF corresponding to max. or min.
+## intensity ('dIepr_over_dB' or other name/label). To select a region / span / interval
+## (limits are `xlim`, `xlim[1]` <=> 'start',`xlim[2]` <=> 'end')
+## of single spectrum in which the extreme ("max" or "min") needs to be found
+Xextreme_Spec  <-  function(data.spectrum,
+                            x = "B_mT",
+                            Intensity = "dIepr_over_dB",
+                            xlim,
+                            extreme){
+  if (extreme == "min"){
+    x.min  <-  data.spectrum  %>%
+      filter(between(.data[[x]],xlim[1],xlim[2]))  %>%
+      filter(.data[[Intensity]] == min(.data[[Intensity]]))  %>%
+      pull(.data[[x]])
+    #
+    return(round(x.min,digits = 2))
+    #
+  }
+  if (extreme == "max"){
+    x.max  <-  data.spectrum  %>%
+      filter(between(.data[[x]],xlim[1],xlim[2]))  %>%
+      filter(.data[[Intensity]] == max(.data[[Intensity]]))  %>%
+      pull(.data[[x]])
+    #
+    return(round(x.max,digits = 2))
+    #
+  }
+}
