950f06aa459379b42eaf86f1a8f56467db448e8a
JanT <jantar40@protonmail.com>
2022-12-19 12:45:10 +0100

    `3D interact.`=> added general `x` + params. renamed

--- R/plot_EPR_Specs3D_interact.R
+++ R/plot_EPR_Specs3D_interact.R
@@ -7,22 +7,22 @@
 #'
 #'
 #' @param data.time.spectra tbc
-#' @param B tbc
+#' @param x tbc
 #' @param Intensity tbc
 #' @param time tbc
 #' @param plot.type tbc
 #' @param scheme.color tbc
 #' @param contour.labels tbc
 #' @param Blim tbc
-#' @param x.label tbc
-#' @param y.label tbc
-#' @param z.label tbc
-#' @param x.bg.color tbc
-#' @param x.grid.color tbc
-#' @param y.bg.color tbc
-#' @param y.grid.color tbc
-#' @param z.bg.color tbc
-#' @param z.grid.color tbc
+#' @param xlab tbc
+#' @param ylab tbc
+#' @param zlab tbc
+#' @param bg.color.x tbc
+#' @param grid.color.x tbc
+#' @param bg.color.y tbc
+#' @param grid.color.y tbc
+#' @param bg.color.z tbc
+#' @param grid.color.z tbc
 #' @param output.matrix.table tbc
 #'
 #'
@@ -41,22 +41,22 @@
 #'
 #'
 plot_EPR_Specs3D_interact <- function(data.time.spectra,
-                                    B = "B_mT",
+                                    x = "B_mT",
                                     Intensity = "dIepr_over_dB",
                                     time = "time_s",
                                     plot.type = "surface",
                                     scheme.color = "Viridis",
                                     contour.labels = FALSE,
                                     Blim,
-                                    x.label = "<i>B</i> (mT)",
-                                    y.label = "<i>Time</i> (s)",
-                                    z.label = "d <i>I</i><sub>EPR</sub> / d <i>B</i> (p.d.u.)",
-                                    x.bg.color = "rgb(220, 220,220)",
-                                    x.grid.color = "rgb(255, 255, 255)",
-                                    y.bg.color = "rgb(220, 220,220)",
-                                    y.grid.color = "rgb(255, 255, 255)",
-                                    z.bg.color = "rgb(220, 220,220)",
-                                    z.grid.color = "rgb(255, 255, 255)",
+                                    xlab = "<i>B</i> (mT)",
+                                    ylab = "<i>Time</i> (s)",
+                                    zlab = "d <i>I</i><sub>EPR</sub> / d <i>B</i> (p.d.u.)",
+                                    bg.color.x = "rgb(220, 220,220)",
+                                    grid.color.x = "rgb(255, 255, 255)",
+                                    bg.color.y = "rgb(220, 220,220)",
+                                    grid.color.y = "rgb(255, 255, 255)",
+                                    bg.color.z = "rgb(220, 220,220)",
+                                    grid.color.z = "rgb(255, 255, 255)",
                                     output.matrix.table = FALSE){
   #
   ## `time` as factor
@@ -64,9 +64,9 @@ plot_EPR_Specs3D_interact <- function(data.time.spectra,
   #
   ## convert data from 'long' to 'wide' table format & finally to matrix
   Intensity_matrix <- data.time.spectra %>%
-    dplyr::select(.data[[time]],.data[[B]],.data[[Intensity]]) %>%
+    dplyr::select(.data[[time]],.data[[x]],.data[[Intensity]]) %>%
     tidyr::pivot_wider(names_from = .data[[time]],values_from = .data[[Intensity]]) %>%
-    dplyr::select(-.data[[B]]) %>%
+    dplyr::select(-.data[[x]]) %>%
     as.matrix()
   ## transpose matrix in order to present 3D spectra properly
   Intensity_matrix <- t(Intensity_matrix)
@@ -76,13 +76,13 @@ plot_EPR_Specs3D_interact <- function(data.time.spectra,
     dplyr::group_by(.data[[time]]) %>%
     dplyr::group_keys()
   #
-  ## select B data frame column for `xaxis` within 3D plot
-  B_select_df <- data.time.spectra %>%
+  ## select x data frame column for `xaxis` within 3D plot
+  X_select_df <- data.time.spectra %>%
     dplyr::filter(.data[[time]] == .data[[time]][1])
   #
   ## own 3D plot (different types "surface","contour","")
   if (plot.type == "surface"){
-    final_plot <- plotly::plot_ly(x = ~B_select_df[[B]],
+    final_plot <- plotly::plot_ly(x = ~X_select_df[[x]],
                           y = ~time_select_df[[time]],
                           z = ~Intensity_matrix,
                           type = plot.type,
@@ -97,25 +97,25 @@ plot_EPR_Specs3D_interact <- function(data.time.spectra,
                         ) %>%
                           plotly::layout(
                             scene = list(
-                              xaxis = list(title = list(text = x.label),
-                                           gridcolor = x.grid.color,
+                              xaxis = list(title = list(text = xlab),
+                                           gridcolor = grid.color.x,
                                            showbackground = TRUE,
-                                           backgroundcolor = x.bg.color,
+                                           backgroundcolor = bg.color.x,
                                            range = Blim),
-                              yaxis = list(title = list(text = y.label),
-                                           gridcolor = y.grid.color,
+                              yaxis = list(title = list(text = ylab),
+                                           gridcolor = grid.color.y,
                                            showbackground = TRUE,
-                                           backgroundcolor = y.bg.color),
-                              zaxis = list(title = list(text = z.label),
-                                           gridcolor = z.grid.color,
+                                           backgroundcolor = bg.color.y),
+                              zaxis = list(title = list(text = zlab),
+                                           gridcolor = grid.color.z,
                                            showbackground = TRUE,
-                                           backgroundcolor = z.bg.color)
+                                           backgroundcolor = bg.color.z)
                           )) %>%
-                          plotly::colorbar(title = z.label)
+                          plotly::colorbar(title = zlab)
   }
   #
   if (plot.type == "contour"){
-    final_plot <- plotly::plot_ly(x = ~B_select_df[[B]],
+    final_plot <- plotly::plot_ly(x = ~X_select_df[[x]],
                           y = ~time_select_df[[time]],
                           z = ~Intensity_matrix,
                           type = plot.type,
@@ -125,11 +125,11 @@ plot_EPR_Specs3D_interact <- function(data.time.spectra,
                             showlabels = contour.labels
                           )) %>%
                           plotly::layout(
-                            xaxis = list(title = list(text = x.label),
+                            xaxis = list(title = list(text = xlab),
                                          range = Blim),
-                            yaxis = list(title = list(text = y.label))
+                            yaxis = list(title = list(text = ylab))
                           ) %>%
-                          plotly::colorbar(title = z.label)
+                          plotly::colorbar(title = zlab)
   }
   #
   ## matrix table output
