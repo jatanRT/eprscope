490cdb97574f8b9325c38402715d4e5909e11a4e
JanT <jantar40@protonmail.com>
2023-02-24 14:08:48 +0100

    `string` bug rel. to present EPR interact fixed

--- R/plot_EPR_present_interact.R
+++ R/plot_EPR_present_interact.R
@@ -1,7 +1,7 @@
 #
 #' Presents/Saves Interactive Plotly-Plot/-Graph Depending on Output Document Format
 #'
-#' @param plot.name tbc
+#' @param plot tbc
 #' @param size.width tbc
 #' @param size.height tbc
 #' @param size.resolv.f tbc
@@ -22,21 +22,21 @@
 #' @importFrom knitr pandoc_to
 #' @importFrom htmlwidgets saveWidget
 #' @importFrom webshot2 webshot
-plot_EPR_present_interact <- function(plot.name,
+plot_EPR_present_interact <- function(plot,
                                       size.width = 700,
                                       size.height = 500,
                                       size.resolv.f = 2){
   if(knitr::pandoc_to("pdf","docx")){
-    saveWidget(widget = plot.name,
-               file = paste0(plot.name,".html"),
+    saveWidget(widget = plot,
+               file = paste0(deparse(substitute(plot)),".html"),
                selfcontained = F)
-    webshot(url = paste0(plot.name,".html"),
-            file = paste0(plot.name,".png"),
+    webshot(url = paste0(deparse(substitute(plot)),".html"),
+            file = paste0(deparse(substitute(plot)),".png"),
             delay = 2,
             vwidth = 700,
             vheight = 500,
             zoom = 2)
   } else{
-    return(plot.name)
+    return(plot)
   }
 }
