b5e89285bd4b83f528deab07a1f508f97f0e2be3
janinoterko <janitt24@duck.com>
2023-02-26 20:18:08 +0100

    EPR present interact code updated/rewritten

--- R/plot_EPR_present_interact.R
+++ R/plot_EPR_present_interact.R
@@ -1,7 +1,7 @@
 #
 #' Presents/Saves Interactive Plotly-Plot/-Graph Depending on Output Document Format
 #'
-#' @param plot tbc
+#' @param p tbc
 #' @param size.width tbc
 #' @param size.height tbc
 #' @param size.resolv.f tbc
@@ -22,31 +22,30 @@
 #' @importFrom knitr pandoc_to is_latex_output
 #' @importFrom htmlwidgets saveWidget
 #' @importFrom webshot2 webshot
-plot_EPR_present_interact <- function(plot,
+plot_EPR_present_interact <- function(p,
                                       size.width = 700,
                                       size.height = 500,
                                       size.resolv.f = 2){
-  if(knitr::is_latex_output()){
-    saveWidget(widget = plot,
-               file = paste0(deparse(substitute(plot)),".html"),
-               selfcontained = F)
-    webshot(url = paste0(deparse(substitute(plot)),".html"),
-            file = paste0(deparse(substitute(plot)),".png"),
-            delay = 2,
-            vwidth = 700,
-            vheight = 500,
-            zoom = 2)
-  } else if(knitr::pandoc_to(fmt = "docx")){
-    saveWidget(widget = plot,
-               file = paste0(deparse(substitute(plot)),".html"),
-               selfcontained = F)
-    webshot(url = paste0(deparse(substitute(plot)),".html"),
-            file = paste0(deparse(substitute(plot)),".png"),
-            delay = 2,
-            vwidth = 700,
-            vheight = 500,
-            zoom = 2)
+  #
+  ## widget and webshot
+  widget <- saveWidget(widget = p,
+                       file = paste0(deparse(substitute(p)),".html"),
+                       selfcontained = FALSE)
+  webshot <- webshot(url = paste0(deparse(substitute(p)),".html"),
+                     file = paste0(deparse(substitute(p)),".png"),
+                     delay = 2,
+                     vwidth = size.width,
+                     vheight = size.height,
+                     zoom = size.resolv.f)
+  #
+  ## Conditions
+  if (knitr::is_latex_output()){
+    return(widget)
+    return(webshot)
+  } else if (knitr::pandoc_to(fmt = "docx")){
+    return(widget)
+    return(webshot)
   } else{
-    plot
+    return(p)
   }
 }
