827201db544ddb5ce66e8a89bc1124fa496eb56c
janinoterko <janitt24@duck.com>
2023-04-08 20:10:41 +0200

    `double integ` replaced by ` sigmoid integ`
    in order to avoid confusing the double integral in case of integration of already originally single integrated spectrum (in such case this is actually the single integral)

--- R/eval_integ_EPR_Spec.R
+++ R/eval_integ_EPR_Spec.R
@@ -10,6 +10,9 @@
 #'  Therefore, depending on \eqn{B} unit (either \code{"G"} or \code{"mT"}) the resulting integral data
 #'  have to be optionally (in case of \code{"mT"}) multiplied by factor of \code{10} because
 #'  \eqn{1 \text{mT}\equiv 10 \text{G}}. Such correction is already included in the `R` function/script.
+#'  Instead of `double integral/integ.` the term `sigmoid integral/integ.` is used. `Double integral`
+#'  \strong{in the case of originally single integrated EPR spectrum} (see \code{data.spectrum} and \code{Intensity})
+#'  \strong{is confusing. In such case the EPR spectrum is integrated only once.}
 #'
 #'
 #' @param data.spectrum Spectrum data frame/table with magnetic flux density (in \code{mT}
@@ -17,7 +20,7 @@
 #'   \code{Index} column may be already present as well.
 #' @param B Character/String pointing to magnetic flux density \code{column} (in the original
 #'   \code{data.spectrum}) either in \code{millitesla} or in \code{Gauss}, that is \code{B = "B_mT"}
-#'   or \code{B = "BField"}...etc or \code{B = "B_G"} (\strong{default}).
+#'   or \code{B = "Field"}...etc or \code{B = "B_G"} (\strong{default}).
 #' @param Intensity Character/String pointing to \code{column} of either derivative
 #'   (e.g. \code{Intensity = "dIepr_over_dB"}, \strong{default}) or single integrated EPR
 #'   spectrum (e.g. \code{Intensity = "single_Integrated"}) within the actual data frame \code{data.spectrum}.
@@ -33,18 +36,21 @@
 #'   of the selected \eqn{B} region, e.g. like `BpeaKlim = c(3535.4,3555.4)`.
 #' @param poly.degree Numeric, degree of polynomial function used to fit the baseline under the single integrated
 #'   curve of the original EPR spectrum.
-#' @param double.integ Logical, whether to present (column in data frame) the double integral of \emph{dIepr_over_dB},
-#'   which is required for quantitative analysis, \strong{default}: \code{double.integ = FALSE}.
+#' @param sigmoid.integ Logical, whether to present (column in data frame) the double integral or single
+#'   integral (if the \code{data.spectrum} and \code{Intesity} are already in single integrated form),
+#'   in sigmoidal shape, which is required for quantitative analysis,
+#'   \strong{default}: \code{sigmoid.integ = FALSE}.
 #' @param output.vecs Logical, whether the `integral` \code{columns} are presented within the entire
 #'   data frame (\code{output.vecs = FALSE}, \strong{default}) or called as a vectors or list for
-#'   additional processing by \pkg{dplyr}.
+#'   additional processing of spectral data series by \pkg{dplyr}.
 #'
 #'
 #' @return The integration results may be divided into following types depending on the above-described
 #'   parameters/arguments. Generally, they are either data frames incl. the original data and the integrals
 #'   (\code{output.vecs = FALSE}) or vectors corresponding to individual baseline corrected/uncorrected
 #'   integrals (\code{output.vecs = TRUE}). This is especially useful for spectral (time) series EPR data,
-#'   which can be handily processed \code{\link[dplyr]{group_by}} using `pipe` operators (\code{\link[magrittr]{\%>\%}}).
+#'   which can be handily processed by \code{\link[dplyr]{group_by}} using the
+#'   `pipe` operators (\code{\link[magrittr]{\%>\%}}).
 #'   \itemize{
 #'   \item Data frame/table including the EPR spectral data (general \emph{Intensity}
 #'   (integrated or derivative) \emph{vs} \eqn{B}) as well as its corresponding \code{single}
@@ -54,22 +60,22 @@
 #'   of polynomial baseline (fitted to experimental baseline without peak). Single integrals are referred either
 #'   to derivative or already single integrated EPR spectra where corrected integral \code{column}
 #'   id denoted as \code{single_Integ_correct}. This is the case for \code{correct.integ = TRUE}
-#'   and \code{double.integ = FALSE} + \code{output.vecs = FALSE}.
-#'   \item The above-described data frame/table with \code{single} and \code{double} integral \strong{column}
-#'   (\code{double_Integ}) essential for quantitative analysis. This is the case for \code{output.vecs = FALSE}
+#'   and \code{sigmoid.integ = FALSE} + \code{output.vecs = FALSE}.
+#'   \item The above-described data frame/table with \code{single} and \code{double/sigmoid} integral \strong{column}
+#'   (\code{sigmoid_Integ}) essential for quantitative analysis. This is the case for \code{output.vecs = FALSE}
 #'   and \code{correct.integ = FALSE}.
-#'   \item Data frame/table in case of \code{correct.integ = TRUE}, \code{double.integ = TRUE}
+#'   \item Data frame/table in case of \code{correct.integ = TRUE}, \code{sigmoid.integ = TRUE}
 #'   and \code{output.vecs = FALSE}. It contains the original data (as in all previous cases) + corrected
-#'   single integral (\code{single_Integ_correct}) and double integral (\code{double_Integ}) which
-#'   is evaluated from the baseline corrected single one. Therefor such double integral is suitable for
-#'   the accurate determination of radical (paramagnetic centers) amount.
+#'   single integral (\code{single_Integ_correct}) and double/sigmoid integral (\code{sigmoid_Integ}) which
+#'   is evaluated from the baseline corrected single one. Therefore such double/sigmoid integral
+#'   is suitable for the accurate determination of radical (paramagnetic centers) amount.
 #'   \item Numeric vector corresponding to baseline uncorrected (or corrected) single integral
-#'   in case of \code{double.integ = FALSE} + \code{output.vecs = TRUE}.
-#'   \item List of numeric vectors corresponding to  `single` (corrected or uncorrected denpending
-#'   on \code{correct.integ} parameter/argument) and `double` integrals (\code{double.integ = TRUE}).
+#'   in case of \code{sigmoid.integ = FALSE} + \code{output.vecs = TRUE}.
+#'   \item List of numeric vectors corresponding to  `single` (corrected or uncorrected depending
+#'   on \code{correct.integ} parameter/argument) and `sigmoid` integrals (\code{sigmoid.integ = TRUE}).
 #'   In such case the integrals are called by \code{list} names like \code{...[["single"]]}
-#'   (or \code{...$single}), corresponding to single integral (or integrated form of EPR spectrum),
-#'   or by \code{...[["double"]]} (or \code{...$double}) corresponding to doubly integrated
+#'   (or \code{...$single}), corresponding to single integral (or already original integrated form of EPR spectrum),
+#'   or by \code{...[["sigmoid"]]} (or \code{...$sigmoid}) corresponding to double integrated
 #'   (or single integrated EPR spectra originally presented already in single integrals) EPR spectral data.
 #'   }
 #'
@@ -77,8 +83,8 @@
 #' @examples
 #' \dontrun{
 #' ## Evaluation of single corrected (by polynomial degree = 3) as well as double
-#' ## integrals corresponding to original derivative EPR spectrum.
-#' ## Integrals are presented as a columns within the en tire data frame
+#' ## (sigmoid) integrals corresponding to original derivative EPR spectrum.
+#' ## Integrals are presented as a columns within the entire data frame
 #' eval_integ_EPR_Spec(EPR_spectral_data_table,
 #'                     B = "FieldB",
 #'                     B.unit = "mT"
@@ -86,16 +92,17 @@
 #'                     correct.integ = TRUE,
 #'                     BpeaKlim = c(349,350),
 #'                     poly.degree = 3,
-#'                     double.integ = TRUE)
+#'                     sigmoid.integ = TRUE)
 #'
-#' ## Double integral evaluation corresponding to original non-corrected
-#' ## integral/intensity (non-derivative EPR signal). Double Integral
+#' ## Integral evaluation corresponding to original non-corrected
+#' ## integral/intensity (non-derivative EPR signal). The Integral
 #' ## (which is actually single integral of the original form, however
-#' ## possesses the sigmoidal curve form) is exported into vectors list
+#' ## possesses the sigmoidal curve shape) is exported into vectors list
 #' eval_integ_EPR_Spec(EPR_spectral_data_table,
 #'                     B = "B_G",
 #'                     B.unit = "G",
-#'                     double.integ = TRUE,
+#'                     Intensity = "Integrated",
+#'                     sigmoid.integ = TRUE,
 #'                     output.vecs = TRUE)
 #'
 #' }
@@ -115,13 +122,13 @@ eval_integ_EPR_Spec <- function(data.spectrum,
                                 correct.integ = FALSE,
                                 BpeaKlim = NULL,
                                 poly.degree = NULL,
-                                double.integ = FALSE,
+                                sigmoid.integ = FALSE,
                                 output.vecs = FALSE) {
   #
   ## 'Temporary' processing variables
   . <- NULL
   single_Integ <- NULL
-  double_Integ <- NULL
+  sigmoid_Integ <- NULL
   baseline_Integ_fit <- NULL
   baseline_Intens_fit <- NULL
   single_Integ_correct <- NULL
@@ -154,17 +161,17 @@ eval_integ_EPR_Spec <- function(data.spectrum,
   #
   if (any(grepl(Intensity,paste(slct.vec.deriv.EPR.intens,collapse = "|")))) {
     #
-    ## integration depending on `B` unit and delete index afterwards
+    ## integration depending on `B` unit
     if (B.unit == "G") {
       data.spectrum <- data.spectrum %>%
         dplyr::mutate(single_Integ = pracma::cumtrapz(
           .data[[B]],
           .data[[Intensity]]
         )[, 1])
-      if (isFALSE(double.integ)) {
+      if (isFALSE(sigmoid.integ)) {
         data.spectrum <- data.spectrum
       } else {
-        data.spectrum$double_Integ <- pracma::cumtrapz(
+        data.spectrum$sigmoid_Integ <- pracma::cumtrapz(
           data.spectrum[[B]],
           data.spectrum$single_Integ
         )[, 1]
@@ -176,10 +183,10 @@ eval_integ_EPR_Spec <- function(data.spectrum,
           .data[[B]],
           .data[[Intensity]]
         )[, 1] * 10)
-      if (isFALSE(double.integ)) {
+      if (isFALSE(sigmoid.integ)) {
         data.spectrum <- data.spectrum
       } else {
-        data.spectrum$double_Integ <- pracma::cumtrapz(
+        data.spectrum$sigmoid_Integ <- pracma::cumtrapz(
           data.spectrum[[B]],
           data.spectrum$single_Integ
         )[, 1] * 10
@@ -190,20 +197,20 @@ eval_integ_EPR_Spec <- function(data.spectrum,
     #
     ## integration depending on `B` unit
     if (B.unit == "G") {
-      if (isFALSE(double.integ)) {
+      if (isFALSE(sigmoid.integ)) {
         data.spectrum <- data.spectrum
       } else {
-        data.spectrum$double_Integ <- pracma::cumtrapz(
+        data.spectrum$sigmoid_Integ <- pracma::cumtrapz(
           data.spectrum[[B]],
           data.spectrum[[Intensity]]
         )[, 1]
       }
     }
     if (B.unit == "mT") {
-      if (isFALSE(double.integ)) {
+      if (isFALSE(sigmoid.integ)) {
         data.spectrum <- data.spectrum
       } else {
-        data.spectrum$double_Integ <- pracma::cumtrapz(
+        data.spectrum$sigmoid_Integ <- pracma::cumtrapz(
           data.spectrum[[B]],
           data.spectrum[[Intensity]]
         )[, 1] * 10
@@ -247,32 +254,32 @@ eval_integ_EPR_Spec <- function(data.spectrum,
           #
           ## integration depending on `B` unit
           if (B.unit == "G") {
-            if (isFALSE(double.integ)) {
+            if (isFALSE(sigmoid.integ)) {
               data.spectrum <- data.spectrum %>%
                 ## remove previous double integral (if present)
                 `if`(
-                  any(grepl("double_Integ", colnames(data.spectrum))),
-                  dplyr::select(-double_Integ), .
+                  any(grepl("sigmoid_Integ", colnames(data.spectrum))),
+                  dplyr::select(-sigmoid_Integ), .
                 )
             } else {
               ## uncorrected double integral is `overwritten`
-              data.spectrum$double_Integ <- pracma::cumtrapz(
+              data.spectrum$sigmoid_Integ <- pracma::cumtrapz(
                 data.spectrum[[B]],
                 data.spectrum$single_Integ_correct
               )[, 1]
             }
           }
           if (B.unit == "mT") {
-            if (isFALSE(double.integ)) {
+            if (isFALSE(sigmoid.integ)) {
               data.spectrum <- data.spectrum %>%
                 ## remove previous double integral (if present)
                 `if`(
-                  any(grepl("double_Integ", colnames(data.spectrum))),
-                  dplyr::select(-double_Integ), .
+                  any(grepl("sigmoid_Integ", colnames(data.spectrum))),
+                  dplyr::select(-sigmoid_Integ), .
                 )
             } else {
               ## uncorrected double integral is `overwritten`
-              data.spectrum$double_Integ <- pracma::cumtrapz(
+              data.spectrum$sigmoid_Integ <- pracma::cumtrapz(
                 data.spectrum[[B]],
                 data.spectrum$single_Integ_correct
               )[, 1] * 10
@@ -300,30 +307,30 @@ eval_integ_EPR_Spec <- function(data.spectrum,
           #
           ## integration depending on `B` unit
           if (B.unit == "G") {
-            if (isFALSE(double.integ)) {
+            if (isFALSE(sigmoid.integ)) {
               data.spectrum <- data.spectrum %>%
-                ## remove previous double integral (if present)
+                ## remove previous sigmoid integral (if present)
                 `if`(
-                  any(grepl("double_Integ", colnames(data.spectrum))),
-                  dplyr::select(-double_Integ), .
+                  any(grepl("sigmoid_Integ", colnames(data.spectrum))),
+                  dplyr::select(-sigmoid_Integ), .
                 )
             } else {
-              data.spectrum$double_Integ <- pracma::cumtrapz(
+              data.spectrum$sigmoid_Integ <- pracma::cumtrapz(
                 data.spectrum[[B]],
                 data.spectrum$single_Integ_correct
               )[, 1]
             }
           }
           if (B.unit == "mT") {
-            if (isFALSE(double.integ)) {
+            if (isFALSE(sigmoid.integ)) {
               data.spectrum <- data.spectrum %>%
-                ## remove previous double integral (if present)
+                ## remove previous sigmoid integral (if present)
                 `if`(
-                  any(grepl("double_Integ", colnames(data.spectrum))),
-                  dplyr::select(-double_Integ), .
+                  any(grepl("sigmoid_Integ", colnames(data.spectrum))),
+                  dplyr::select(-sigmoid_Integ), .
                 )
             } else {
-              data.spectrum$double_Integ <- pracma::cumtrapz(
+              data.spectrum$sigmoid_Integ <- pracma::cumtrapz(
                 data.spectrum[[B]],
                 data.spectrum$single_Integ_correct
               )[, 1] * 10
@@ -343,12 +350,12 @@ eval_integ_EPR_Spec <- function(data.spectrum,
     #
     ## reorder columns in data frame
     data.spectrum <- data.spectrum %>%
-      dplyr::relocate(double_Integ, .after = tidyselect::last_col())
+      dplyr::relocate(sigmoid_Integ, .after = tidyselect::last_col())
     #
     integrate.results <- data.spectrum
     #
   } else {
-    if (isFALSE(double.integ)) {
+    if (isFALSE(sigmoid.integ)) {
       ## bacause `ifelse()` does not work for vectors
       ## it hast be replaced by `switch()` function, like =>
       ## integrated <- FALSE
@@ -365,7 +372,7 @@ eval_integ_EPR_Spec <- function(data.spectrum,
             data.spectrum$single_Integ,
             data.spectrum$single_Integ_correct
           ),
-          double = data.spectrum$double_Integ
+          sigmoid = data.spectrum$sigmoid_Integ
         )
       }
       if (any(grepl(Intensity,paste(slct.vec.integ.EPR.intens,collapse = "|")))) {
@@ -374,7 +381,7 @@ eval_integ_EPR_Spec <- function(data.spectrum,
             data.spectrum$Intensity,
             data.spectrum$single_Integ_correct
           ),
-          double = data.spectrum$double_Integ
+          sigmoid = data.spectrum$sigmoid_Integ
         )
       }
     }
