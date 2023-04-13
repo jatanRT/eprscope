fcb7c4788ff3d1c656795b4990a2d4e6f9d43ac9
janinoterko <janitt24@duck.com>
2023-04-13 23:25:14 +0200

    update rewrit. fundamental funct. to read spectra

--- R/readEPR_Exp_Specs.R
+++ R/readEPR_Exp_Specs.R
@@ -7,7 +7,7 @@
 #'   which can be easily processed by other R packages (e.g. by \pkg{tidyverse} system), afterwards.
 #'   Spectral data (intensities) are normalized by the common experimental parameters like Q-factor, concentration...etc.
 #'   ASCII files/tables depend on the origin/software used to acquire the EPR spectra. This is mirrored by \code{origin}
-#'   parameter. Time series (time evolution of EPR spectra) can be read by the \code{time.series = TRUE/FALSE} parameter.
+#'   parameter. Time series (time evolution of EPR spectra) can be read by the \code{time.series} parameter.
 #'
 #'
 #' @param path_to_ASC String, path to ASCII file/table (e.g. \code{.txt}, \code{.csv} or \code{.asc})
@@ -32,12 +32,16 @@
 #' @param col.char2num Logical, description...converting character column to numeric format... TBC
 #' @param x Numeric index related to \code{col.names} pointing to independent variable, which corresponds
 #'   to abscissa (\eqn{x}-axis) in spectra or other plots.
+#' @param x.unit Character/String ...TBC
 #' @param Intensity Numeric index related to \code{col.names} pointing to `general` intensity,
 #'   like derivative intensity (`dIepr_over_dB`), integral one (e.g. `single_Integ`), double or sigmoid
 #'   integral (e.g. `Area`)...etc. This corresponds to column/vector which should be presented like
 #'   \eqn{y}-axis in spectra or other plots.
-#' @param convert_B_G2mT Logical (\strong{default}: \code{convert_B_G2mT}) description... convert \eqn{B}
-#'   in Gauss to millitesla
+#' @param time.series Numeric index related to \code{col.names} pointing to `time` column for time series
+#'   EPR spectra or spectral parameters (e.g. like intensity, integral/area) changing upon time.
+#'   \strong{Default}: \code{time.series = NULL}.
+#' @param convert_B_G2mT Logical (\strong{default}: \code{convert_B_G2mT = TRUE}) description...
+#'   convert \eqn{B} in Gauss to millitesla
 #' @param qValue Numeric, Q value (quality factor, number) displayed at specific \code{dB} by spectrometer,
 #'   in case of ` "Xenon" ` software the parameter is included in \code{.DSC} file, \strong{default}:
 #'   \code{qValue = 1}
@@ -45,8 +49,6 @@
 #'   all additional (in addition to \code{qValue}) normalization(s) like e.g. concentration, powder sample
 #'   weight, number of scans, ...etc (\code{norm.vec.add = c(2000,0.5,2)}). \strong{Default}:
 #'   \code{norm.vec.add = NULL}.
-#' @param time.series Boolean, whether the input ASCII data come from the time series experiment
-#'   with the additional \code{time} column, \strong{default}: \code{time.series = FALSE}
 #' @param origin String/Character corresponding to \strong{origin} of the ASCII data, like from
 #'   most common spectrometers (from which are data loaded automatically using the default parameters).
 #'   Options are summarized in the following table (Any other specific `origin` may be added later) =>
@@ -152,11 +154,12 @@ readEPR_Exp_Specs <- function(path_to_ASC,
                               ),
                               col.char2num = FALSE,
                               x = 2,
+                              x.unit = "G",
                               Intensity = 3,
+                              time.series = NULL,
                               convert_B_G2mT = TRUE,
                               qValue = 1,
                               norm.vec.add = NULL,
-                              time.series = FALSE,
                               origin = "xenon") {
   ## 'Temporary' processing variables
   B_G <- NULL
@@ -165,18 +168,19 @@ readEPR_Exp_Specs <- function(path_to_ASC,
   #
   ## general normalization
   norm.vec.add <- norm.vec.add %>% `if`(is.null(norm.vec.add),1, .)
-  norm.const <- prod(sapply(norm.vec.add, function(n) 1/n))
+  norm.multiply.const <- prod(sapply(norm.vec.add, function(n) 1/n))
+  norm.multiply.qValue <- 1/qValue
   #
   ## basic `fread` parameters to read the spectral data
   if (origin == "winepr") {
-    if (isFALSE(time.series)) {
+    if (is.null(time.series)) {
       ## parameter definition
       sep <- sep %>% `if`(sep != "auto", "auto", .)
       header <- header %>% `if`(isTRUE(header),FALSE, .)
       skip <- skip %>% `if`(skip != 3,3, .)
-      na.strings <- NULL
-      select <- NULL
-      drop <- NULL
+      na.strings <- na.strings %>% `if`(!is.null(na.strings),NULL, .)
+      select <- select %>% `if`(!is.null(select),NULL, .)
+      drop <- drop %>% `if`(!is.null(drop),NULL, .)
       encoding <- encoding %>% `if`(encoding != "unknown", "unknown", .)
       fill <- fill %>% `if`(isTRUE(fill),FALSE, .)
       blank.lines.skip <- blank.lines.skip %>% `if`(isTRUE(blank.lines.skip), FALSE, .)
@@ -191,8 +195,8 @@ readEPR_Exp_Specs <- function(path_to_ASC,
       blank.lines.skip <- blank.lines.skip %>% `if`(isFALSE(blank.lines.skip),TRUE, .)
       na.strings <- na.strings %>% `if`(is.null(na.strings),
                                         c("Intensity","X [G]","Y []"), .)
-      select <- NULL
-      drop <- NULL
+      select <- select %>% `if`(!is.null(select),NULL, .)
+      drop <- drop %>% `if`(!is.null(drop),NULL, .)
       encoding <- encoding %>% `if`(encoding != "unknown", "unknown", .)
       #
     }
@@ -202,9 +206,9 @@ readEPR_Exp_Specs <- function(path_to_ASC,
     sep <- sep %>% `if`(sep != "auto", "auto", .)
     header <- header %>% `if`(isTRUE(header),FALSE, .)
     skip <- skip %>% `if`(skip != 1,1, .)
-    na.strings <- NULL
-    select <- NULL
-    drop <- NULL
+    na.strings <- na.strings %>% `if`(!is.null(na.strings),NULL, .)
+    select <- select %>% `if`(!is.null(select),NULL, .)
+    drop <- drop %>% `if`(!is.null(drop),NULL, .)
     encoding <- encoding %>% `if`(encoding != "unknown", "unknown", .)
     fill <- fill %>% `if`(isTRUE(fill),FALSE, .)
     blank.lines.skip <- blank.lines.skip %>% `if`(isTRUE(blank.lines.skip), FALSE, .)
@@ -236,9 +240,9 @@ readEPR_Exp_Specs <- function(path_to_ASC,
                                             fill = fill,
                                             blank.lines.skip = blank.lines.skip
   ) %>%
-    `if`(origin == "winepr" & isTRUE(time.series),
+    `if`(origin == "winepr" & !is.null(time.series),
          dplyr::filter(grepl("Slice",.data[[1]])), .) %>%
-    `if`(origin == "winepr" & isTRUE(time.series),
+    `if`(origin == "winepr" & !is.null(time.series),
          stats::na.omit(), .)
   #
   ## Condition to convert any character column to numeric format
@@ -250,40 +254,26 @@ readEPR_Exp_Specs <- function(path_to_ASC,
     }
   }
   #
-
-  ## TO BE COMPLETED !!
-
-  ## TO BE COMPLETED !!
-
-  if (isFALSE(time.series)) {
-    if (x.unit == "Unitless") {
-      x <- grep("g", col.names, value = TRUE)
-    }
-    if (x.unit == "G" || x.unit == "mT") {
-      x <- grep("B|G|mT|BField|Field", col.names, value = TRUE)[[1]]
-      ## `[[1]]` selection is due to fact that intensity
-      ## could be `dIepr_over_dB` which also has `B` within
-      ## the character string
-      ## `|` \equiv `or`
-    }
-    if (x.unit == "MHz") {
-      x <- grep("rf|RF|MHz|radio|Radio|freq|Freq", col.names, value = TRUE)
-    }
-  } else {
-    if (x.unit == "Unitless") {
-      x <- grep("g", col.names, value = TRUE)
-    }
-    if (x.unit == "G" || x.unit == "mT") {
-      x <- grep("B|G|mT", col.names, value = TRUE)[[1]]
-    }
-    if (x.unit == "MHz") {
-      x <- grep("rf|RF|MHz|radio|Radio|freq|Freq", col.names, value = TRUE)
-    }
-    time <- grep("time|Time|tim|Tim", col.names, value = TRUE)
+  ## `Intensity` and `x` column character string definitions
+  IntensityAxis <- col.names[Intensity]
+  xAxis <- col.names[x]
+  ## new `spectra.data`
+  if (x.unit == "G"){
+    spectra.data <- spectrum.data.origin %>%
+      `if`(isTRUE(convert_B_G2mT),
+           dplyr::mutate(B_mT = .data[[xAxis]]/10), .) %>%
+      dplyr::mutate(!!rlang::quo_name(IntensityAxis) := .data[[IntensityAxis]]*norm.multiply.qValue*norm.multiply.const)
   }
-  #
-  ## select intesity column character string
-  Intensity <- grep("I|Intens|intens|MW_Abs|MW_Intens", col.names, value = TRUE)
+  if (x.unit == "MHz"){
+    spectra.data <- spectrum.data.origin %>%
+      dplyr::mutate(!!rlang::quo_name(IntensityAxis) := .data[[IntensityAxis]]*norm.multiply.const)
+  }
+
+  ## TO BE COMPLETED !!
+
+  ## TO BE COMPLETED !!
+
+
   #
   ##
   if (origin == "xenon" || origin == "txt" || origin == "csv") {
@@ -332,6 +322,6 @@ readEPR_Exp_Specs <- function(path_to_ASC,
     }
   }
   #
-  return(spectrum.data)
+  return(spectra.data)
   #
 }
