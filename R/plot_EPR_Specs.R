96f364934089de69d550729eee71fdb2dfbb0bd8
janinoterko <janitt24@duck.com>
2023-05-08 22:35:53 +0200

    fixing colorscale bug in the main plot funct.

--- R/plot_EPR_Specs.R
+++ R/plot_EPR_Specs.R
@@ -346,7 +346,8 @@ plot_EPR_Specs <- function(data.spectra,
           #
           ## colors definition for the plot
           if (length(line.colors) > 1){
-            plot.vector.colors <- grDevices::colorRampPalette(line.colors)(var2nd.series.len)
+            plot.vector.colors <-
+              grDevices::colorRampPalette(colors = line.colors)(length(data.spectra[[var2nd.series]]))
             #
             simplePlot <- simplePlot.nocolor +
               scale_color_manual(values = plot.vector.colors) +
