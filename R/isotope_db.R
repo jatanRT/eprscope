3945027b463fa1b74a78718076a3aa2f2e7bc5f2
janinoterko <janitt24@duck.com>
2023-05-14 22:23:03 +0200

    documentation to isotope table updated

--- R/isotope_db.R
+++ R/isotope_db.R
@@ -5,15 +5,13 @@
 #' and only it's format was slightly modified. Therefore, IT CONTAINS THE ENTIRE INFORMATION
 #' LIKE THE ORIGINAL DATABASE (see the SOURCE BELOW). For better orientation in ENDOR spectra,
 #' column with the Larmor/ENDOR frequencies (in MHz) at 0.35 T was added according to =>
-#'
-#' \deqn{\nu_{\text{L,ENDOR}}^{} = - \frac{1}{h}\,\mu_{\text{N}}^{}\,g_{\text{n}}^{}\,B}
-#'
+#' \deqn{\nu_{\text{ENDOR}}^{} = - \frac{1}{h}\,\mu_{\text{N}}^{}\,g_{\text{n}}^{}\,B}
 #' where \eqn{h} is the Planck's constant, \eqn{\mu_{\text{N}}^{}} is the nuclear magneton
 #' available from \pkg{constants} package (\code{constants::syms$mun}), \eqn{g_{\text{n}}^{}},
 #' is the nuclear \eqn{g}-factor of the specific nucleus (reported in the data frame as \code{g_Nuclear})
 #' and finally, the \eqn{B = 0.35\,\text{T}} denotes the magnetic flux density.
 #'
-#' @format A data frame with ... rows and 9 variables/columns:
+#' @format A data frame with 351 rows and 9 variables/columns:
 #' \describe{
 #'   \item{No_Proton}{Numeric, proton number.}
 #'   \item{Isotope}{Character string ponting to isotope in format like "14N".}
@@ -21,9 +19,9 @@
 #'   \item{Name}{Character string corresponding to isotope name.}
 #'   \item{Spin}{Numeric, denoting the spin quantum number.}
 #'   \item{g_Nuclear}{Numeric, corresponding to nuclear \eqn{g}-factor (\eqn{g_{\text{n}}^{}}).}
-#'   \item{Abund_Natur_Percent}{Numeric, pointing to natural abundance of an isotope.}
+#'   \item{Abund_Natur_Percent}{Numeric, pointing to natural abundance of an isotope in \eqn{\%}.}
 #'   \item{Q_Barn}{Numeric, corresponding to nuclear quadrupolar moment in \eqn{10^{-28}\,\text{m}^2}.}
-#'   \item{nu_ENDOR_MHz_035T}{Numeric, specific Larmor/ENDOR frequency (\eqn{\nu_{\text{L,ENDOR}}^{}})
+#'   \item{nu_ENDOR_MHz_035T}{Numeric, specific Larmor/ENDOR frequency (\eqn{\nu_{\text{ENDOR}}^{}})
 #'         at \eqn{0.35\,\text{T}}.}
 #' }
 #' @source \url{https://easyspin.org/easyspin/documentation/isotopetable.html}
