52f284e7e46921ab544c357f3c904ccfb142e720
janinoterko <janitt24@duck.com>
2023-10-19 21:59:35 +0200

    new funct. to load example data/spectra

---
+++ R/load_data_example.R
@@ -0,0 +1,32 @@
+#'
+#' Loading Data Files for Package Examples
+#'
+#'
+#' @description
+#' Loads data files which are required to demonstrate the the package functionality
+#' by the examples within documentation.
+#'
+#'
+#' @param file Character string corresponding to file name + extension.
+#'
+#'
+#' @examples
+#' ## List of all files within the "extdata" directory =>
+#' load_data_example()
+#'
+#'
+#'
+#' @export
+#'
+#'
+load_data_example <- function(file = NULL){
+  if (is.null(file)){
+    ## displaying all data files in "extdata"
+    dir(system.file("extdata",package = "eprscope"))
+  } else {
+    ## loading the file(s) from "extdata" directory
+    ## which is transferred one level up by the installation
+    system.file("extdata",file,package = "eprscope",mustWork = TRUE)
+  }
+  #
+}
