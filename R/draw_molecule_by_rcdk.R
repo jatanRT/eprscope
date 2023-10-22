915fd46b707c0d3a2dd1d49c5ab65c623453aa24
janinoterko <janitt24@duck.com>
2023-10-22 23:49:33 +0200

    `draw molecule` added opt. to place `mol.name`+docu

--- R/draw_molecule_by_rcdk.R
+++ R/draw_molecule_by_rcdk.R
@@ -16,9 +16,14 @@
 #' @param type Character string ...tbc... either \code{type = "smiles"}/\code{type = "SMILES"}
 #'   or \code{type = "sdf"}/\code{type = "SDF"}. \strong{Default}: \code{type = "smiles"}.
 #' @param mol.name Character string pointing to name of molecule/compound, e.g. like
-#'   \code{mol.name = c("acetone")} or \code{mol.name = c("PBN")}.
+#'   \code{mol.name = c("acetone")} or \code{mol.name = c("PBN")}. If \code{mol.name = NULL}
+#'   (\strong{default}) a character string "mol. structure viewer" with gray color
+#'   is shown.
 #' @param mol.name.color Character string pointing to displayed font color of the chemical structure
 #'   name. \strong{Default}: \code{mol.name.color = "black"}.
+#' @param mol.name.xy.posit Numeric vector matching the x,y-position of the \code{mol.name}
+#'   label within the image having the size of "(1,1,10,10)" =>
+#'   see \code{\link[graphics]{rasterImage}}.
 #' @param sma Character string allowing to highlight sub-structures using `SMARTS`
 #'   (\strong{SM}ILES \strong{A}rbitrary \strong{T}arget \strong{S}pecification) to highlight
 #'   common substructures in a set of molecules, e.g. like \code{sma = "C=O"}.
@@ -60,13 +65,14 @@
 #'                       type = "smiles",
 #'                       sma = "cN")
 #' #
-#' ## draw N,N,N',N'-tetramethyl-p-phenylenediamine based
-#' ## on `smiles` code character, with hydrogen atoms
-#' ## and "TMPD" name of the molecule
-#' draw_molecule_by_rcdk("CN(C)C1=C([H])C([H])=C(N(C)C)C([H])=C1[H]",
+#' ## draw N,N,N',N'-tetramethyl-p-phenylenediamine (TMPD) radical
+#' ## cation based on `smiles` code character, with hydrogen atoms
+#' ## and molecule name label = "TMPD^(+.)"
+#' draw_molecule_by_rcdk("CN(C)[C+]1C([H])=C([H])[C.]([N](C)C)C([H])=C1[H]",
 #'                       type = "smiles",
-#'                       suppressh = FALSE,
-#'                       mol.name = "TMPD")
+#'                       mol.name = expression(TMPD^(+.)),
+#'                       mol.name.color = "blue",
+#'                       suppressh = FALSE)
 #' #
 #' ## draw N,N,N',N'-tetramethyl-p-phenylenediamine based
 #' ## on `sdf` file path ("TMPD.sdf") with "color on black"
@@ -89,6 +95,7 @@ draw_molecule_by_rcdk <- function(molecule,
                                   type = "smiles", ## or "sdf"
                                   mol.name = NULL,
                                   mol.name.color = "black",
+                                  mol.name.xy.posit = c(8.2,1.2),
                                   sma = NULL,
                                   annotate = "off",
                                   style = "cow",
@@ -140,13 +147,13 @@ draw_molecule_by_rcdk <- function(molecule,
   #
   ## drawing `mol.name`
   if (is.null(mol.name)){
-    graphics::text(x = 5.5,
-                   y = 1.1,
+    graphics::text(x = mol.name.xy.posit[1],
+                   y = mol.name.xy.posit[2],
                    labels = c("mol. structure viewer"),
                    col = "gray")
   } else{
-    graphics::text(x = 5.5,
-                   y = 1.1,
+    graphics::text(x = mol.name.xy.posit[1],
+                   y = mol.name.xy.posit[2],
                    labels = mol.name,
                    col = mol.name.color)
   }
