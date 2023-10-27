e0418fbd3243b59282127b4bc046798aa4af444f
JanT <jantar40@protonmail.com>
2023-10-27 10:06:09 +0200

    similar functs. condensed into one corresp. script

---
+++ R/eval_gFactors.R
@@ -0,0 +1,321 @@
+#'
+#' Basic Calculation of \eqn{g}-Factor
+#'
+#'
+#' @family Evaluations
+#'
+#'
+#' @description Calculation of {g}-factor according to fundamental formula.
+#'   The magnetic flux density (\code{B}) and microwave frequency (\code{nu},\eqn{\nu})
+#'   can be entered with common units like \code{G} (Gauss) \code{mT}
+#'   (millitesla) or \code{T} (tesla) as well as \code{GHz} or \code{Hz}, respectively.
+#'   The Planck constant (\eqn{h}) and Bohr magneton (\eqn{\mu_{B}}) are included
+#'   in \code{\link[constants]{syms}} function and their values are taken by \code{syms$h}
+#'   and \code{syms$mub} commands, respectively.
+#'
+#'
+#' @param nu Numeric, microwave Frequency
+#' @param nu.unit Character string, frequency unit defined by \code{"GHz"} or \code{"Hz"}, \strong{default}: \code{nu.unit = "GHz"}
+#' @param B Numeric, magnetic flux density
+#' @param B.unit Character string, magnetic flux density unit in \code{"G"} or \code{"mT"} or \code{"T"}, \strong{default}:
+#'   \code{B.unit = "mT"}
+#'
+#' @return g-value from \eqn{(\nu h)/(\mu_{B} B)}. For variables and constants =>
+#'   see description above
+#'
+#' @examples
+#' eval_gFactor(9.8020458,
+#'              nu.unit = "GHz",
+#'              350.214,
+#'              B.unit = "mT")
+#' #
+#' eval_gFactor(nu = 9.8020458e+9,
+#'              nu.unit = "Hz",
+#'              B = 3502.14,
+#'              B.unit = "G")
+#' #
+#' eval_gFactor(9.5421,"GHz",0.333251,"T")
+#'
+#' @export
+eval_gFactor <- function(nu,
+                         nu.unit = "GHz",
+                         B,
+                         B.unit = "mT") {
+  #
+  ## Fundamental Physical Constants
+  Planck.const <- constants::syms$h
+  Bohr.magnet <- constants::syms$mub
+  #
+  ## Conditions to calculate `g`
+  if (nu.unit == "GHz" & B.unit == "mT") {
+    g <- (Planck.const * nu * 1e+9) / (Bohr.magnet * B * 0.001)
+  } else if (nu.unit == "Hz" & B.unit == "mT") {
+    g <- (Planck.const * nu) / (Bohr.magnet * B * 0.001)
+  } else if (nu.unit == "Hz" & B.unit == "G") {
+    g <- (Planck.const * nu) / (Bohr.magnet * B * 0.0001)
+  } else if (nu.unit == "GHz" & B.unit == "G") {
+    g <- (Planck.const * nu * 1e+9) / (Bohr.magnet * B * 0.0001)
+  } else if (nu.unit == "GHz" & B.unit == "T") {
+    g <- (Planck.const * nu * 1e+9) / (Bohr.magnet * B)
+  } else if (nu.unit == "Hz" & B.unit == "T") {
+    g <- (Planck.const * nu) / (Bohr.magnet * B)
+  }
+  #
+  return(round(g, digits = 5))
+  #
+}
+#
+#
+#
+#
+#' Calculation of \eqn{g}-factor ("Position") from the EPR Spectrum/Data
+#'
+#'
+#' @family Evaluations
+#'
+#'
+#' @description Calculation of g-value according to fundamental formula (\code{\link{eval_gFactor}}).
+#'   \eqn{g}-related magnetic flux density (like \eqn{B_{iso}} or \eqn{B_{center}}) is directly taken
+#'   from the EPR spectrum. If positive and negative derivative intensities of the spectral line are similar
+#'   and their distance from the middle of the spectrum equals, the \eqn{B_{iso}} should be be considered,
+#'   otherwise the \eqn{B_{center}} must be taken into account. In case of integrated EPR spectrum/data
+#'   the \eqn{B_{max}} is used for the \eqn{g}-value evaluation.
+#'
+#'
+#' @param data.spectrum Spectrum data frame/table where the magnetic flux density (in \code{mT} or \code{G}) column
+#'   can be labeled as \code{Field} or \code{B_G} and that of the derivative intensity as \code{dIepr_over_dB}
+#'   or single integrated intensity like \code{Integrated_Intensity}, \code{index} column can be included as well.
+#' @param nu.GHz Numeric, microwave frequency in \code{GHz}
+#' @param B.unit description tbc
+#' @param B Character string pointing to magnetic flux density \code{column} of EPR spectrum data frame
+#'   \code{data.spectrum} either in \code{millitesla} or in \code{Gauss}, that is \code{B = "B_mT"} (\strong{default})
+#'   or \code{B = "B_G"} or \code{B = "B_G_Sim"} to include simulated EPR spectra as well
+#' @param Intensity Character string pointing to \code{intensity column} if other than \code{dIepr_over_dB}
+#'   name/label is used (e.g. for simulated spectra), \strong{default}: \code{Intesity = "dIepr_over_dB"}
+#' @param lineSpecs.form Character string describing either \code{"derivative"} (\strong{default}) or \code{"integrated"}
+#'   (i.e. \code{"absorption"} which can be used as well) line form of the analyzed EPR spectrum/data.
+#' @param Blim Numeric vector, magnetic flux density in \code{mT}/\code{G} corresponding to border limits
+#'   of the selected \eqn{B} region, e.g. like `Blim = c(3495.4,3595.4)`. \strong{Default}: \code{Blim = NULL} (corresponding
+#'   to entire `B` range).
+#' @param iso Logical, whether to calculate the \eqn{g}-factor from the \eqn{B} value corresponding to
+#'   that between the \code{min.} and \code{max.} derivative intensities (\code{dIepr_over_dB}, that is \eqn{g_{iso}}
+#'   (this is the \strong{default}: \code{iso = TRUE}), or by finding the the \eqn{B} value corresponding
+#'   to \code{dIepr_over_dB = 0} (close/near zero, which is \code{iso = FALSE})
+#'
+#' @return Numeric \eqn{g_{iso}}-value ('iso' = 'isotropic') according to \eqn{(\nu h)/(\mu_{B} B)}
+#'
+#'
+#' @examples
+#' \dontrun{
+#' eval_gFactor_Spec(data.spectrum,
+#'                   9.82451,
+#'                   B.unit = "mT",
+#'                   "Field_mT",
+#'                   Intensity = "dIepr_over_dB_Sim",
+#'                   c(349.8841,351.112))
+#' #
+#' eval_gFactor_Spec(data.spectrum,
+#'                   nu.GHz = 9.82451,
+#'                   B.unit = "G",
+#'                   B = "B_G",
+#'                   Blim = c(3498.841,3511.12),
+#'                   iso = FALSE)
+#' #
+#' eval_gFactor_Spec(data.spectrum,
+#'                   9.91024,
+#'                   B.unit = "G",
+#'                   B = "B_G_Sim",
+#'                   Intensity = "Integral_Intensity",
+#'                   lineSpecs.form = "integrated",
+#'                   c(3499,3501))
+#' }
+#'
+#'
+#' @export
+#'
+#'
+#' @importFrom dplyr filter select mutate pull between near
+eval_gFactor_Spec <- function(data.spectrum,
+                              nu.GHz,
+                              B.unit = "G",
+                              B = "B_G",
+                              Intensity = "dIepr_over_dB",
+                              lineSpecs.form = "derivative",
+                              Blim = NULL,
+                              iso = TRUE) {
+  ## 'Temporary' processing variables
+  AbsIntens <- NULL
+  . <- NULL
+  #
+  ## Define limits if `Blim = NULL` take the entire data region
+  ## otherwise use predefined vector
+  data.B.region <- c(min(data.spectrum[[B]]), max(data.spectrum[[B]]))
+  Blim <- Blim %>% `if`(is.null(Blim), data.B.region, .)
+  #
+  ## B minimum & maximum
+  B.min <- data.spectrum %>%
+    dplyr::filter(dplyr::between(.data[[B]], Blim[1], Blim[2])) %>%
+    dplyr::filter(.data[[Intensity]] == min(.data[[Intensity]])) %>%
+    dplyr::pull(.data[[B]])
+  #
+  B.max <- data.spectrum %>%
+    dplyr::filter(dplyr::between(.data[[B]], Blim[1], Blim[2])) %>%
+    dplyr::filter(.data[[Intensity]] == max(.data[[Intensity]])) %>%
+    dplyr::pull(.data[[B]])
+  ## B between minimum and maximum of dIepr_over_dB:
+  if (isTRUE(iso)) {
+    if (lineSpecs.form == "derivative") {
+      B.center <- (B.min + B.max) / 2
+      ## B at dIepr_over_dB = 0 (near 0, see next comment on `B.center`):
+    }
+    if (lineSpecs.form == "integrated" || lineSpecs.form == "absorption") {
+      B.center <- B.max
+    }
+  } else {
+    if (lineSpecs.form == "derivative") {
+      ## Find the value B, corresponding to Intensity very close to 0 (tolerance max(Intensity)/100)
+      B.center <- data.spectrum %>%
+        dplyr::filter(dplyr::between(.data[[B]], B.max, B.min)) %>%
+        dplyr::mutate(AbsIntens = abs(.data[[Intensity]])) %>%
+        dplyr::filter(dplyr::near(AbsIntens, 0, tol = max(.data[[Intensity]]) / 2)) %>%
+        dplyr::filter(AbsIntens == min(AbsIntens)) %>%
+        dplyr::pull(.data[[B]])
+    }
+    if (lineSpecs.form == "integrated" || lineSpecs.form == "absorption") {
+      B.center <- B.max
+    }
+  }
+  ## g -value calculation:
+  Planck.const <- constants::syms$h
+  Bohr.magnet <- constants::syms$mub
+  g.precurs <- (Planck.const * nu.GHz * 1e+9) / (Bohr.magnet * B.center)
+  #
+  ## Conditions for B column, the name should contain ("B", "mT" or "G"):
+  if (B.unit == "mT") {
+    g <- g.precurs / 1e-3
+  }
+  if (B.unit == "G") {
+    g <- g.precurs / 1e-4
+  }
+  #
+  return(round(g, digits = 5))
+  #
+}
+#
+#
+#
+#
+#' Calculation of \eqn{g}-factor from Quantum Chemical Computational Output
+#'
+#'
+#' @family Evaluations and Quantum Chemistry
+#'
+#'
+#' @description \eqn{g}-Values (3 principal ones) are presented in a form of differences from the \eqn{g_e}.
+#' Therefore the function takes these values and calculates the entire \eqn{g}-values or parses
+#' the corresponding mean value from `Gaussian` or `ORCA` output.
+#'
+#'
+#' @param path_to_QCHoutput Character string corresponding to path of `Gaussian` or `ORCA` output text files
+#'   incl. all \eqn{g}-values. \code{\link[base]{file.path}} can be applied to get the full/relative path.
+#' @param mean Logical, whether to calculate the \code{mean value/iso} from principal components,
+#'   \strong{default}: \code{mean = TRUE}, or save the entire vector with all the components.
+#' @param origin Character string pointing to origin of DFT EPR calculation parameters <=> which
+#'   software package was used. Only two values are available => \code{"gaussian"} (\strong{default})
+#'   or \code{"orca"}.
+#'
+#'
+#' @return Numeric mean \eqn{g}-factor value from principal difference (from \eqn{g_e}) components
+#'   calculated by QCH methods (e.g. by DFT) or numeric vector with principal \eqn{g}-components
+#'   if \code{mean = FALSE}
+#'
+#'
+#' @examples
+#' \dontrun{
+#' eval_gFactor_QCHcomp("./InputData/DFTcomputs/RadicalDFToutput.inp.log")
+#' eval_gFactor_QCHcomp(file.path(".","DFT_calculations","EPRorcaRadical.out"),
+#'                     mean = FALSE,
+#'                     origin = "orca")
+#' }
+#'
+#'
+#' @export
+#'
+#'
+eval_gFactor_QCHcomp <- function(path_to_QCHoutput,
+                                 mean = TRUE,
+                                 origin = "gaussian") {
+  #
+  ## reading the output files from GAUSSIAN or ORCA
+  qchfile <- readLines(path_to_QCHoutput)
+  #
+  ## g-Value indicator based on `origin`
+  if (origin == "gaussian") {
+    gval.indicator <- "g shifts relative"
+    start.read.line <- grep(gval.indicator, qchfile)
+    qchfile.select.g <- qchfile[start.read.line + 1]
+    #
+    ## character line split into string elements
+    qchfile.select.g <- stringr::str_split(qchfile.select.g,
+                                           pattern = "[[:space:]]+"
+    )
+    #
+    ## select only number strings
+    ## `str_split` results in list therefore it has to be unlisted
+    qchfile.select.g <- unlist(qchfile.select.g)
+    #
+    vector.string.dg <- c(
+      qchfile.select.g[3],
+      qchfile.select.g[5],
+      qchfile.select.g[7]
+    )
+    #
+    #
+    ## numeric values (in `Gaussian` they are already presented in ppm)
+    vector.dg <- as.numeric(as.character(vector.string.dg))
+  }
+  if (origin == "orca") {
+    gval.indicator <- "Delta-g"
+    start.read.line <- grep(gval.indicator, qchfile)
+    qchfile.select.g <- qchfile[start.read.line]
+    #
+    ## character line split into string elements
+    qchfile.select.g <- stringr::str_split(qchfile.select.g,
+                                           pattern = "[[:space:]]+"
+    )
+    #
+    ## select only number strings
+    qchfile.select.g <- unlist(qchfile.select.g) ## `str_split` results in list
+    #
+    vector.string.dg <- c(
+      qchfile.select.g[3],
+      qchfile.select.g[4],
+      qchfile.select.g[5]
+    )
+    #
+    ## numeric values (in `ORCA` they are in form 1e-6)
+    vector.dg <- as.numeric(as.character(vector.string.dg)) * 1e6
+  }
+  #
+  ## Delete `qchfile` => it is not required anymore
+  rm(qchfile)
+  #
+  ## g-factor for free electron (g.e) from `constants` package
+  ## round the g.e to 6 decimal places
+  g.e <- round(-constants::syms$gem, digits = 6)
+  #
+  ## g-vector from shifts (`deltas` or `d`) and g.e
+  delta_g_vec <- vector.dg
+  g_vec <- g.e + delta_g_vec * 1e-6
+  #
+  ## whether to calculate the mean value or get the g-vector
+  ## as it is
+  if (isTRUE(mean)) {
+    gValue <- mean(g_vec)
+  } else {
+    gValue <- g_vec
+  }
+  #
+  return(gValue)
+  #
+}
---
+++ R/readEPR_params_slct.R
@@ -0,0 +1,358 @@
+#' Read the \strong{Selected} EPR Instrumental Parameters and Information
+#'
+#'
+#' @family Data Reading
+#'
+#'
+#' @description
+#'  Function takes the \strong{selected} instrumental parameters or information
+#'  from \code{.DSC} or \code{.par} file of an EPR Spectrum (written by the \code{Xenon}
+#'  or \code{WinEpr} Software, respectively)
+#'
+#'
+#' @param path_to_DSC_or_par String, path to \code{.DSC} or \code{.par} file including all instrumental
+#'   parameters provided by the EPR machine, path can be provided by \code{\link[base]{file.path}}
+#' @param string String, within the \code{.DSC} or \code{.par} (at the line beginning) file
+#'   corresponding to instrumental parameter,
+#'  following \strong{strings are defined} (\strong{in parenthesis for "winepr" software}):
+#'  \tabular{ll}{
+#'   ---------------- \tab ----------------------------- \cr
+#'   \strong{String} \tab \strong{Instrumental Parameter} \cr
+#'   ---------------- \tab ----------------------------- \cr
+#'    "OPER" ("JON") \tab  operator (of the EPR instrument) \cr
+#'    "CMNT" ("JCO") \tab  comment (in order to describe the measurement) \cr
+#'    "DATE" ("JDA") \tab  date (when the EPR spectrum was recorded) \cr
+#'    "TIME" ("JTM") \tab  time (when the EPR spectrum was recorded) \cr
+#'    "SAMP" \tab   name/decsript. of the sample \cr
+#'    "B0MF" \tab  modulation frequency in \code{Hz} \cr
+#'    "MWFQ"  ("MF") \tab microwave frequency in \code{Hz} (\code{GHz}) \cr
+#'    "QValue" \tab  recorded quality-Factor (required for intensity norm.) \code{unitless} \cr
+#'    "A1CT" ("HCF") \tab central field (B) in \code{T} (\code{G}) \cr
+#'    "A1SW" ("HSW") \tab  sweep width in \code{T} (\code{G}) \cr
+#'    "STMP" ("TE")  \tab  temperature in \code{K} \cr
+#'    "B0MA" ("RMA") \tab  modulation amplitude in \code{T} (\code{G}) \cr
+#'    "AVGS" ("JSD") \tab  number of accumulations for each spectrum \cr
+#'    "A1RS" ("RES") \tab  number of points/resolution \cr
+#'    "MWPW" ("MP") \tab microwave power in \code{W} (\code{mW}) \cr
+#'    "SPTP" ("RCT") \tab  conversion time in \code{s} (\code{ms}) \cr
+#'    "RCTC" ("RTC") \tab  time constant in \code{s} (ms) \cr
+#'    "RCAG" ("RRG") \tab signal receiver gain in \code{dB} (unitless) \cr
+#'    "ConvFact" \tab conversion factor/instr. calibration constant for quantitative
+#'    analysis \code{unitless} \cr
+#'    ---------------- \tab ----------------------------- \cr
+#'  }
+#' @param origin String, corresponding to software which was used to acquire the EPR spectra
+#'   on BRUKER spectrometers, because the files are slightly different depending on whether they were recorded
+#'   by the windows based softw. ("WinEpr",\code{origin = "winepr"}) or by the Linux one ("Xenon"),
+#'   \strong{default}: \code{origin = "xenon"}
+#'
+#' @return Numeric or String/Character (e.g. date or comment) corresponding to selected (\code{slct}) instrumental
+#'   parameter applied to record the EPR spectra.
+#'
+#'
+#' @examples
+#' \dontrun{
+#' ## Reading modulation amplitude from 'Xenon' spectrometer file
+#' readEPR_param_slct(path_to_DSC_or_par,
+#'                    string = "B0MA")
+#'
+#' ## Reading Q Value from 'Xenon' spectrometer file
+#' read_param_slct(file.path(".",
+#'                           "dir_DSC",
+#'                           "EPR_spectrum.DSC"),
+#'                 string = "QValue")
+#'
+#' ## Reading `date` from 'WinEPR' spectrometer file
+#' read_param_slct(file.path(".",
+#'                           "dir_par",
+#'                           "EPR_spectrum.par"),
+#'                 string = "JDA",
+#'                 origin = "winepr")
+#' }
+#'
+#'
+#' @export
+#'
+readEPR_param_slct <- function(path_to_DSC_or_par,
+                               string,
+                               origin = "xenon") {
+  #
+  ## path corresponds to file (.DSC) from which the params. are read
+  ## string is the selected 'string' pattern e.g. like "QValue" or "MWFQ"
+  sel.str.line <- grep(paste0("^",string), readLines(path_to_DSC_or_par), value = TRUE)
+  #
+  ## such line is then separated (split) into two ('n = 2') string parts
+  ## by 'str_split' comming from 'stringr' pckg.
+  sel.str.split <- stringr::str_split(sel.str.line, "[[:space:]]+", n = 2)
+  #
+  ## the result is list, therefore select the second list element ('[[1]][2]'),
+  ## therefore unlist the `sel.str.split`
+  sel.str.split <- unlist(sel.str.split)
+  #
+  if (origin == "xenon") {
+    if (string == "OPER" || string == "CMNT" || string == "DATE" || string == "TIME" || string == "SAMP") {
+      param.slct <- as.character(sel.str.split[2])
+    } else {
+      param.slct <- as.double(sel.str.split[2])
+    }
+  }
+  if (origin == "winepr") {
+    if (string == "JON" || string == "JCO" || string == "JDA" || string == "JTM") {
+      param.slct <- as.character(sel.str.split[2])
+    } else {
+      param.slct <- as.double(sel.str.split[2])
+    }
+  }
+  #
+  return(param.slct)
+  #
+}
+#
+#
+#
+#
+#' Read the \strong{Selected} Instrumental Parameters Relevant to \strong{Time Series} Experiment
+#'
+#'
+#' @family Data Reading
+#'
+#'
+#' @description
+#'  Function takes the \strong{selected} instrumental parameters relevant to \strong{time series ("kinetic")}
+#'  experiment from \code{.DSC} or \code{.par} file of an EPR Spectrum (written by the `Xenon`
+#'  or `WinEpr` software, respectively). These parameters are required for the time correction of EPR
+#'  spectra, see \code{\link{correct_time_Exp_Specs}}
+#'
+#'
+#' @param path_to_DSC_or_par String, path to \code{.DSC} or \code{.par} file including all instrumental
+#'   parameters provided by the EPR machine
+#' @param origin String, corresponding to software which was used to acquire the EPR spectra
+#'   on BRUKER spectrometers, because the files are slightly different depending on whether they were recorded
+#'   by the windows based softw. ("WinEpr",\code{origin = "winepr"}) or by the Linux one ("Xenon"),
+#'   \strong{default}: \code{origin = "xenon"}
+#'
+#'
+#' @return List containing:
+#'
+#'   * number of scans (\code{Nscans})
+#'
+#'   * sweep time (\code{swTime}) required for time correction during the \code{2D_Field_Delay}
+#'     experiment ("kinetic" time series)
+#'
+#'   * number of points (\code{Npoints})
+#'
+#'
+#' @examples
+#' \dontrun{
+#' readEPR_params_slct_kin(path_to_DSC_or_par)
+#' readEPR_params_slct_kin(file.path(".",
+#'                                   "dir_par",
+#'                                   "EPR_spectrum.par"),
+#'                         origin = "winepr")
+#' }
+#'
+#' @export
+#'
+#'
+readEPR_params_slct_kin <- function(path_to_DSC_or_par, origin = "xenon") {
+  ## Load all required parameters from `.DSC` or `.par`
+  if (origin == "xenon") {
+    resol <- readEPR_param_slct(path_to_DSC_or_par,
+                                string = "A1RS",
+                                origin = origin
+    )
+    convTime <- readEPR_param_slct(path_to_DSC_or_par,
+                                   string = "SPTP",
+                                   origin = origin
+    )
+    NScans <- readEPR_param_slct(path_to_DSC_or_par,
+                                 string = "NbScansToDo",
+                                 origin = origin
+    )
+    ## for kinetic measurements "AVGS" doesn't work, therefore select "NbScansToDo"
+  }
+  if (origin == "winepr") {
+    resol <- readEPR_param_slct(path_to_DSC_or_par,
+                                string = "RES",
+                                origin = origin
+    )
+    convTime <- readEPR_param_slct(path_to_DSC_or_par,
+                                   string = "RCT",
+                                   origin = origin
+    )
+    NScans <- readEPR_param_slct(path_to_DSC_or_par,
+                                 string = "JSD",
+                                 origin = origin
+    )
+  }
+  sweeptime <- resol * convTime
+  #
+  return(list(Nscans = NScans, swTime = sweeptime, Npoints = resol))
+  #
+}
+#
+#
+#
+#
+#' Read the \strong{Selected} Instrumental Parameters Relevant to EPR Quantitative Analysis
+#'
+#'
+#' @family Data Reading
+#'
+#'
+#' @description
+#'   Reading the \code{.DSC} or \code{.par} file to extract the important parameters like
+#'   "modulation amplitude", "temperature", "microwave power" as well as "microwave frequency"
+#'   which are are required for absolute quantitative analysis of the EPR spectra (\eqn{\equiv}
+#'   radical or paramagnetic species number determination).
+#'
+#'
+#'
+#' @param path_to_DSC_or_par Character string, path (also provided by \code{\link[base]{file.path}})
+#'   to \code{.DSC} or \code{.par} (depending on OS, see \code{origin} parameter)
+#'   \code{text} files including all instrumental parameters and provided by the EPR machine.
+#' @param origin String, corresponding to software which was used to acquire the EPR spectra
+#'   on BRUKER spectrometers, because the files are slightly different depending on whether they
+#'   were recorded by the windows based softw. ("WinEpr",\code{origin = "winepr"}) or by the Linux
+#'   one ("Xenon"). \strong{Default}: \code{origin = "xenon"}.
+#'
+#'
+#' @return List consisting of:
+#'
+#'   * \code{BmmT}, "modulation amplitude" value  in `mT`
+#'
+#'   * \code{PmW}, "microwave source power" value in `mW`
+#'
+#'   * \code{TK}, "temperature" value in `K`
+#'
+#'   * \code{mwGHz}, "microwave frequency" value in `GHz`.
+#'
+#'
+#' @examples
+#' \dontrun{
+#' TODO
+#' TODO
+#' }
+#'
+#'
+#' @export
+#'
+#'
+## function to read instrumental parameters from `.DSC` or `.par`
+## required for quantification
+readEPR_params_slct_quant <- function(path_to_DSC_or_par,
+                                      origin = "xenon"){
+  #
+  ## reading the table and extracting values form table
+  params.df <- readEPR_params_tabs(path_to_DSC_or_par,origin = origin)$params
+  #
+  Bm.mT <- params.df %>%
+    dplyr::filter(.data$Parameter == "Modulation Amplitude") %>%
+    dplyr::pull(dplyr::all_of(c("Value")))
+  Bm.mT <- round(Bm.mT,digits = 3)
+  #
+  P.mW <- params.df %>%
+    dplyr::filter(.data$Parameter == "Power") %>%
+    dplyr::pull(dplyr::all_of(c("Value")))
+  P.mW <- round(P.mW,digits = 2)
+  #
+  Temp.K <- params.df %>%
+    dplyr::filter(.data$Parameter == "Temperature") %>%
+    dplyr::pull(dplyr::all_of(c("Value")))
+  Temp.K <- round(Temp.K)
+  #
+  nu.GHz <- params.df %>%
+    dplyr::filter(.data$Parameter == "Frequency") %>%
+    dplyr::pull(dplyr::all_of(c("Value")))
+  nu.GHz <- round(nu.GHz,digits = 7)
+  ## not required anymore =>
+  rm(params.df)
+  #
+  named.params.list <- list(BmmT = Bm.mT,PmW = P.mW,TK = Temp.K,mwGHz = nu.GHz)
+  return(named.params.list)
+}
+#
+#
+#
+#
+#' Read the \strong{Selected} Instrumental Parameters Relevant to \strong{Simulation} of EPR Spectra
+#'
+#'
+#' @family Data Reading
+#'
+#'
+#' @description
+#'   Reading the \code{.DSC} or \code{.par} file to extract the important parameters like
+#'   "sweep width", "central field", "number of points" as well as "microwave frequency"
+#'   which are are required for the simulations of EPR spectra (see \code{\link{eval_sim_EPR_iso}}).
+#'
+#'
+#'
+#' @param path_to_DSC_or_par Character string, path (also provided by \code{\link[base]{file.path}})
+#'   to \code{.DSC} or \code{.par} (depending on OS, see \code{origin} parameter)
+#'   \code{text} files including all instrumental parameters and provided by the EPR machine.
+#' @param origin String, corresponding to software which was used to acquire the EPR spectra
+#'   on BRUKER spectrometers, because the files are slightly different depending on whether they
+#'   were recorded by the windows based softw. ("WinEpr",\code{origin = "winepr"}) or by the Linux
+#'   one ("Xenon"). \strong{Default}: \code{origin = "xenon"}.
+#' @param B.unit Character string pointing to unit of magnetic flux density which is the output
+#'   `unit`, \code{"G"} (`Gauss`) or \code{"mT"} (`millitesla`), for \code{"sweep width"}
+#'   and \code{"central field"} (see \code{\link{eval_sim_EPR_iso}}).
+#'   \strong{Default}: \code{B.unit = "G"}.
+#'
+#'
+#' @return List consisting of:
+#'
+#'   * \code{Bcf}, "central field" value in `B.unit`
+#'
+#'   * \code{Bsw}, "sweep width" value in `B.unit`)
+#'
+#'   * \code{Npoints}, "number of points" value
+#'
+#'   * \code{mwGHz}, "microwave frequency" value in `GHz`
+#'
+#'
+#' @examples
+#' \dontrun{
+#' TODO
+#' TODO
+#' }
+#'
+#'
+#' @export
+#'
+#'
+## function to read instrumental parameters from `.DSC` or `.par`
+## required for simulation
+readEPR_params_slct_sim <- function(path_to_DSC_or_par,
+                                    origin = "xenon",
+                                    B.unit = "G"){
+  #
+  ## reading the table and extracting values form table
+  params.df <- readEPR_params_tabs(path_to_DSC_or_par,origin = origin)$params
+  #
+  B.CF <- params.df %>%
+    dplyr::filter(.data$Parameter == "Central Field") %>%
+    dplyr::pull(dplyr::all_of(c("Value"))) %>% convert_B(B.unit = "mT",B.2unit = B.unit)
+  B.CF <- round(B.CF,digits = 3)
+  #
+  B.SW <- params.df %>%
+    dplyr::filter(.data$Parameter == "Sweep Width") %>%
+    dplyr::pull(dplyr::all_of(c("Value"))) %>% convert_B(B.unit = "mT",B.2unit = B.unit)
+  B.SW <- round(B.SW,digits = 3)
+  #
+  Npoints <- params.df %>%
+    dplyr::filter(.data$Parameter == "Number of Points") %>%
+    dplyr::pull(dplyr::all_of(c("Value")))
+  Npoints <- round(Npoints)
+  #
+  nu.GHz <- params.df %>%
+    dplyr::filter(.data$Parameter == "Frequency") %>%
+    dplyr::pull(dplyr::all_of(c("Value")))
+  nu.GHz <- round(nu.GHz,digits = 7)
+  ## not required anymore =>
+  rm(params.df)
+  #
+  named.params.list <- list(Bcf = B.CF,Bsw = B.SW,Npoints = Npoints,mwGHz = nu.GHz)
+  return(named.params.list)
+}
