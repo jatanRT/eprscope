a198705c4bdcf931097bc96d0d3165d462effc0d
janinoterko <janitt24@duck.com>
2023-11-13 23:43:59 +0100

    added epr iso sim. fit examples+resids. in plot out

--- R/eval_sim_EPR_isoFit.R
+++ R/eval_sim_EPR_isoFit.R
@@ -401,14 +401,12 @@ eval_sim_EPR_isoFit <- function(data.spectrum.expr,
       labs(title = "EPR Simulation Fit",
            color = NULL,
            x = NULL,
-           y = bquote(d~italic(I)[EPR]~~"/"~~d~italic(B)~~~"("~p.d.u.~")")) +
-      plot_theme_In_ticks(axis.title.size = 14,
-                          axis.text.size = 13) +
+           y = bquote(d*italic(I)[EPR]~~"/"~~d*italic(B)~~~"("~p.d.u.~")")) +
+      plot_theme_In_ticks() +
       scale_x_continuous(sec.axis = dup_axis(name = "",labels = NULL)) +
       scale_y_continuous(sec.axis = dup_axis(name = "",labels = NULL)) +
       theme(legend.title = element_text(size = 13),
-            legend.text = element_text(size = 11),
-            axis.title.y = element_text(hjust = 0))
+            legend.text = element_text(size = 11))
       #
       plot.sim.expr.lower <- ggplot(data = data.sim.expr.resid) +
         geom_line(aes(x = .data[[paste0("B_",B.unit)]],
@@ -417,9 +415,8 @@ eval_sim_EPR_isoFit <- function(data.spectrum.expr,
                   linewidth = 0.75) +
         labs(title = "Residuals",
              x = bquote(italic(B)~~"("~.(B.unit)~")"),
-             y = NULL) +
-        plot_theme_In_ticks(axis.title.size = 14,
-                            axis.text.size = 13) +
+             y = bquote(Diff.~d*italic(I)[EPR]~~"/"~~d*italic(B)~~~"("~p.d.u.~")")) +
+        plot_theme_In_ticks() +
         scale_x_continuous(sec.axis = dup_axis(name = "",labels = NULL)) +
         scale_y_continuous(sec.axis = dup_axis(name = "",labels = NULL))
       #
