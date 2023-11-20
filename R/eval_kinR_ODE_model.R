7a5845f91cb44d499bdbd805fa1023d75844c810
JanT <jantar40@protonmail.com>
2023-11-20 14:39:41 +0100

    ODE kinetic models extended + docu. updated

--- R/eval_kinR_ODE_model.R
+++ R/eval_kinR_ODE_model.R
@@ -6,9 +6,12 @@
 #'
 #'
 #' @description
-#'   The primary aim of the function is to get an idea of theoretical quantitative (e.g. like
-#'   concentration/amount/integral intensity) kinetic profile for various model reactions where
+#'   Theoretical quantitative kinetic profiles (e.g. like concentration/amount/integral intensity)
+#'   as well as fitting of the experimental data for various model reactions where
 #'   the radical(s) ("R") are involved in the processes.
+#'
+#'
+#' @details
 #'   Secondary purpose is to fit the experimental EPR spectral time series outputs, e.g. like integral
 #'   intensity (area under the spectral curve) \emph{vs} time in order to gather the rate constants
 #'   (\eqn{k}) of radical ("R") formation and/or decomposition. Quantitative kinetic profiles
@@ -21,29 +24,29 @@
 #'   article}.
 #'   The most applied kinetic models for radical reactions in EPR spectroscopy are summarized below
 #'   (see also \code{model.react} function argument).
-#'   \tabular{ccl}{
-#'   -------------------- \tab | \tab -------------------- \cr
-#'   \strong{`model.react`} \tab | \tab \strong{Description} \cr
-#'   -------------------- \tab | \tab -------------------- \cr
-#'   \code{"(x=1)R --> [k1] B"} \tab | \tab Basic irreversible forward reaction,
+#'   \tabular{ll}{
+#'   ------------------------------- \tab ----------------------------------------------------- \cr
+#'   \strong{`model.react`} \tab \strong{Description} \cr
+#'   ------------------------------- \tab ----------------------------------------------------- \cr
+#'   \code{"(x=1)R --> [k1] B"} \tab Basic irreversible forward reaction,
 #'   e.g. like irrev. dimerization (if \code{"(x=2)"}). \cr
-#'   \code{"(x=1)A --> [k1] R"} \tab | \tab Basic irreversible radical formation,
+#'   \code{"(x=1)A --> [k1] R"} \tab Basic irreversible radical formation,
 #'   e.g. like irrev. \eqn{\text{e}^-} transfer. \cr
-#'   \code{"(x=1)A --> [k1] (x=1)R --> [k2] C"} \tab | \tab Consecutive reactions,
+#'   \code{"(x=1)A --> [k1] (x=1)R <==> [k2] [k3] C"} \tab Consecutive reactions,
 #'   e.g. like comproportionation (or homogeneous
-#'   \eqn{\text{e}^-} transfer, for \code{"(x=2)"}) + follow up irrev. dimerization. \cr
-#'   \code{"(x=1)R <==> [k1] [k2] B"} \tab | \tab Basic reversible radical quenching,
+#'   \eqn{\text{e}^-} transfer, for \code{"(x=2)"}) + follow up reversible dimerization. \cr
+#'   \code{"(x=1)R <==> [k1] [k2] B"} \tab Basic reversible radical quenching,
 #'   e.g. like rev. (\eqn{\pi-\pi} dimerization). \cr
-#'   \code{"(x=1)A <==> [k1] [k2] (x=1)R"} \tab | \tab Basic reversible radical formation,
+#'   \code{"(x=1)A <==> [k1] [k2] (x=1)R"} \tab Basic reversible radical formation,
 #'   e.g. from rev.
 #'   comproportionation/disproportionation (\eqn{\text{A}^{++} + \text{A}^0 \xrightleftharpoons ~ 2\text{R}^{.+}},
 #'   for \code{"(x=2)"}). \cr
-#'   \code{"A [k1] <-- (x=1)R --> [k2] B"} \tab | \tab Basic parallel reactions,
+#'   \code{"A [k1] <-- (x=1)R --> [k2] B"} \tab Basic parallel reactions,
 #'   e.g. like radical decomposition and isomerization or two radical diffusion forming different product
 #'   than without diffusion like recombination (\code{"(x=2)"}). \cr
-#'   \code{"(x=1)A + (y=1)B --> [k1] R"} \tab | \tab Radical formation by chemical reaction like oxidation,
+#'   \code{"(x=1)A + (y=1)B --> [k1] R"} \tab Radical formation by chemical reaction like oxidation,
 #'   reduction or spin trapping (transient radical is not visible within the common EPR time scale). \cr
-#'   -------------------- \tab | \tab -------------------- \cr
+#'   ------------------------------- \tab ----------------------------------------------------- \cr
 #'   }
 #'
 #'
@@ -54,23 +57,23 @@
 #'   \href{https://www.ctan.org/pkg/mhchem?lang=en}{\eqn{\LaTeX} `mhchem` package}.
 #'   Following reaction schemes are the most common used to describe the integral intensity and/or radical
 #'   concentration/amount changes during the EPR time series experiment.
-#'   \tabular{lcl}{
-#'   ------------------------ \tab | \tab -------------------- \cr
-#'   \strong{Reaction Scheme} \tab | \tab \strong{`model.react`} \cr
-#'   ------------------------ \tab | \tab -------------------- \cr
-#'   \eqn{(x=1)\text{R} \xrightarrow{k_1} \text{B}} \tab | \tab \code{"(x=1)R --> [k1] B"} \cr
-#'    \eqn{(x=1)\text{A} \xrightarrow{k_1} \text{R}} \tab | \tab \code{"(x=1)A --> [k1] R"} \cr
-#'    \eqn{(x=1)\text{A} \xrightarrow{k_1} (x=1)\text{R} \xrightarrow{k_2} \text{C}} \tab | \tab
-#'    \code{"(x=1)A --> [k1] (x=1)R --> [k2] C"} \cr
-#'    \eqn{(x=1)\text{R} \xrightleftharpoons[k_2]{k_1} \text{B}} \tab | \tab
+#'   \tabular{ll}{
+#'   ------------------------------ \tab ------------------------------------------------------ \cr
+#'   \strong{Reaction Scheme} \tab \strong{`model.react`} \cr
+#'   ------------------------------ \tab ------------------------------------------------------ \cr
+#'   \eqn{(x=1)\text{R} \xrightarrow{k_1} \text{B}} \tab \code{"(x=1)R --> [k1] B"} \cr
+#'    \eqn{(x=1)\text{A} \xrightarrow{k_1} \text{R}} \tab \code{"(x=1)A --> [k1] R"} \cr
+#'    \eqn{(x=1)\text{A} \xrightarrow{k_1} (x=1)\text{R} \xrightleftharpoons[k_3]{k_2} \text{C}} \tab
+#'    \code{"(x=1)A --> [k1] (x=1)R <==> [k2] [k3] C"} \cr
+#'    \eqn{(x=1)\text{R} \xrightleftharpoons[k_2]{k_1} \text{B}} \tab
 #'    \code{"(x=1)R <==> [k1] [k2] B"} \cr
-#'    \eqn{(x=1)\text{A} \xrightleftharpoons[k_2]{k_1} (x=1)\text{R}} \tab | \tab
+#'    \eqn{(x=1)\text{A} \xrightleftharpoons[k_2]{k_1} (x=1)\text{R}} \tab
 #'    \code{"(x=1)A <==> [k1] [k2] (x=1)R"} \cr
-#'    \eqn{\text{A} \xleftarrow{k_1} (x=1)\text{R} \xrightarrow{k_2} \text{B}} \tab | \tab
+#'    \eqn{\text{A} \xleftarrow{k_1} (x=1)\text{R} \xrightarrow{k_2} \text{B}} \tab
 #'    \code{"A [k1] <-- (x=1)R --> [k2] B"} \cr
-#'    \eqn{(x=1)\text{A} + (y=1)\text{B} \xrightarrow{k_1} \text{R}} \tab | \tab
+#'    \eqn{(x=1)\text{A} + (y=1)\text{B} \xrightarrow{k_1} \text{R}} \tab
 #'    \code{"(x=1)A + (y=1)B --> [k1] R"} \cr
-#'    ------------------------ \tab | \tab -------------------- \cr
+#'    ----------------------------- \tab ------------------------------------------------------ \cr
 #'   }
 #'   Couple of examples are also given in the description. The function is relatively flexible and enables
 #'   later addition of any other reaction schemes describing the EPR time series experiments
@@ -94,22 +97,22 @@
 #'   The initial values are denoted as \code{qvar0X} (e.g. qvar0R for radical or qvar0A for reactant).
 #'   The components of \code{kin.params} numeric vector depend on \code{model.react} like summarized in the
 #'   following table =>
-#'   \tabular{ccl}{
-#'   -------------------- \tab | \tab ------------------- \cr
-#'   \strong{`model.react`} \tab | \tab \strong{Essential `kin.params` components} \cr
-#'   -------------------- \tab | \tab ------------------- \cr
-#'   \code{"(x=1)R --> [k1] B"} \tab | \tab \code{k1}, \code{qvar0R}, (\code{n}) \cr
-#'   \code{"(x=1)A --> [k1] R"} \tab | \tab \code{k1}, \code{qvar0A}, \code{qvar0R}, (\code{n}) \cr
-#'   \code{"(x=1)A --> [k1] (x=1)R --> [k2] C"} \tab | \tab \code{k1}, \code{k2}, \code{qvar0A},
+#'   \tabular{ll}{
+#'   ----------------------------- \tab ----------------------------------- \cr
+#'   \strong{`model.react`} \tab \strong{Essential `kin.params` components} \cr
+#'   ----------------------------- \tab ----------------------------------- \cr
+#'   \code{"(x=1)R --> [k1] B"} \tab \code{k1}, \code{qvar0R}, (\code{n}) \cr
+#'   \code{"(x=1)A --> [k1] R"} \tab \code{k1}, \code{qvar0A}, \code{qvar0R}, (\code{n}) \cr
+#'   \code{"(x=1)A --> [k1] (x=1)R <==> [k2] [k3] C"} \tab \code{k1}, \code{k2}, \code{k3} \code{qvar0A},
 #'   \code{qvar0R}, \code{qvar0C}, (\code{n}) \cr
-#'   \code{"(x=1)R <==> [k1] [k2] B"} \tab | \tab \code{k1}, \code{k2}, \code{qvar0R},
+#'   \code{"(x=1)R <==> [k1] [k2] B"} \tab \code{k1}, \code{k2}, \code{qvar0R},
 #'   \code{qvar0B}, (\code{n}) \cr
-#'   \code{"(x=1)A <==> [k1] [k2] (x=1)R"} \tab | \tab \code{k1}, \code{k2}, \code{qvar0A},
+#'   \code{"(x=1)A <==> [k1] [k2] (x=1)R"} \tab \code{k1}, \code{k2}, \code{qvar0A},
 #'   \code{qvar0R}, (\code{n}) \cr
-#'   \code{"A [k1] <-- (x=1)R --> [k2] B"} \tab | \tab \code{k1}, \code{k2}, \code{qvar0R}, (\code{n}) \cr
-#'   \code{"(x=1)A + (y=1)B --> [k1] R"} \tab | \tab \code{k1}, \code{qvar0A}, \code{qvar0B},
+#'   \code{"A [k1] <-- (x=1)R --> [k2] B"} \tab \code{k1}, \code{k2}, \code{qvar0R}, (\code{n}) \cr
+#'   \code{"(x=1)A + (y=1)B --> [k1] R"} \tab \code{k1}, \code{qvar0A}, \code{qvar0B},
 #'   \code{qvar0R}, (\code{n}) \cr
-#'   -------------------- \tab | \tab ------------------- \cr
+#'   ----------------------------- \tab ----------------------------------- \cr
 #'   }
 #' @param data.expr R data frame containing the integral intensities/areas under the curves calculated
 #'   using the \strong{experimental data} as well as time column. These two essential columns
@@ -146,9 +149,10 @@
 #' #
 #' ## consecutive reactions and preesenting plot
 #' kin.test.02 <-
-#'  eval_kinR_ODE_model(model.react = "(x=1)A --> [k1] (x=1)R --> [k2] C",
+#'  eval_kinR_ODE_model(model.react = "(x=1)A --> [k1] (x=1)R <==> [k2] [k3] C",
 #'                      kin.params = c(k1 = 0.01,
 #'                                     k2 = 0.015,
+#'                                     k3 = 0.0075,
 #'                                     qvar0A = 0.02,
 #'                                     qvar0R = 0.002,
 #'                                     qvar0C = 0))
@@ -377,11 +381,12 @@ eval_kinR_ODE_model <- function(model.react = "(x=1)R --> [k1] B", ## for x = 1,
       tidyr::pivot_longer(!time, names_to = "Species", values_to = "qvar") %>%
       dplyr::arrange(time)
   }
-  if (grepl("^\\(x=.*A --> \\[k1\\] \\(x=.*R --> \\[k2\\] C$", model.react)) {
+  if (grepl("^\\(x=.*A --> \\[k1\\] \\(x=.*R <==> \\[k2\\] \\[k3\\] C$", model.react)) {
     ## functions for derivative solution of kinetic equation
     react_rates_diff_03 <- function(t, qvar, kin.params, c_x) {
       k1 <- kin.params$k1
       k2 <- kin.params$k2
+      k3 <- kin.params$k3
       if (c_x == "n") {
         n <- kin.params$n
       }
@@ -390,13 +395,13 @@ eval_kinR_ODE_model <- function(model.react = "(x=1)R --> [k1] B", ## for x = 1,
       ## differential equations
       if (inherits(c_x, "numeric")) {
         rate[1] <- -k1 * (qvar["A"])^(c_x)
-        rate[2] <- k1 * (qvar["A"])^(c_x) - k2 * (qvar["R"])^(c_x)
-        rate[3] <- k2 * (qvar["R"])^(c_x)
+        rate[2] <- k1 * (qvar["A"])^(c_x) - k2 * (qvar["R"])^(c_x) + k3 * (qvar["C"])^(c_x)
+        rate[3] <- k2 * (qvar["R"])^(c_x) - k3 * (qvar["C"])^(c_x)
       }
       if (c_x == "n") {
         rate[1] <- -k1 * (qvar["A"])^n
-        rate[2] <- k1 * (qvar["A"])^n - k2 * (qvar["R"])^n
-        rate[3] <- k2 * (qvar["R"])^n
+        rate[2] <- k1 * (qvar["A"])^n - k2 * (qvar["R"])^n + k3 * (qvar["C"])^n
+        rate[3] <- k2 * (qvar["R"])^n - k3 * (qvar["C"])^n
       }
       ## derivative as a list
       return(list(rate))
@@ -411,6 +416,7 @@ eval_kinR_ODE_model <- function(model.react = "(x=1)R --> [k1] B", ## for x = 1,
     ## rate constant and other params. definition
     k1 <- kin.params["k1"]
     k2 <- kin.params["k2"]
+    k3 <- kin.params["k3"]
     if (stoichiom_coeff(model.react) == "n") {
       n <- kin.params["n"]
     }
@@ -421,7 +427,7 @@ eval_kinR_ODE_model <- function(model.react = "(x=1)R --> [k1] B", ## for x = 1,
         y = qvar0, times = t,
         c_x = stoichiom_coeff(model.react),
         func = react_rates_diff_03,
-        parms = list(k1 = k1, k2 = k2, n = n)
+        parms = list(k1 = k1, k2 = k2, k3 = k3, n = n)
       )
     }
     if (inherits(stoichiom_coeff(model.react), "numeric")) {
@@ -429,7 +435,7 @@ eval_kinR_ODE_model <- function(model.react = "(x=1)R --> [k1] B", ## for x = 1,
         y = qvar0, times = t,
         c_x = stoichiom_coeff(model.react),
         func = react_rates_diff_03,
-        parms = list(k1 = k1, k2 = k2)
+        parms = list(k1 = k1, k2 = k2, k3 = k3)
       )
     }
     #
@@ -601,13 +607,17 @@ eval_kinR_ODE_model <- function(model.react = "(x=1)R --> [k1] B", ## for x = 1,
         n <- kin.params$n
       }
       ## initial conditions
-      rate <- rep(0, times = 1)
+      rate <- rep(0, times = 3)
       ## differential equations
       if (inherits(c_x, "numeric")) {
         rate[1] <- -k1 * (qvar)^(c_x) - k2 * (qvar)^(c_x)
+        rate[2] <- k1 * (qvar)^(c_x)
+        rate[3] <- k2 * (qvar)^(c_x)
       }
       if (c_x == "n") {
         rate[1] <- -k1 * (qvar)^n - k2 * (qvar)^n
+        rate[2] <- k1 * (qvar)^n
+        rate[3] <- k2 * (qvar)^n
       }
       ## derivative as a list
       return(list(rate))
