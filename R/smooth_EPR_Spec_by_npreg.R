468f4904e12a0c5fe049a2220e579866da9ae066
JanT <jantar40@protonmail.com>
2023-12-19 18:10:37 +0100

    function to smooth spectra updated=> docu.+val.added

--- R/smooth_EPR_Spec_by_npreg.R
+++ R/smooth_EPR_Spec_by_npreg.R
@@ -6,20 +6,81 @@
 #'
 #'
 #' @description
-#'   A short description...Smoothing of EPR spectra...TBC...
+#'   Smoothing of the EPR spectra by non-parametric fitting a smoothing spline \code{\link[npreg]{ss}}
+#'   from \pkg{npreg} package.
 #'
 #'
-#' @param data.spectr Data fame object ...TBC...
-#' @param B Character string ...TBC...
-#' @param B.unit Character string ...TBC...
-#' @param lineSpecs.form Character string ...TBC...
-#' @param Intensity Character string ...TBC...
-#' @param method Character string ...TBC...
-#' @param output.vec Logical, description ...TBC...
-#' @param ... additional arguments passed to the function (see also \code{\link[npreg]{ss}}).
+#' @details
+#'   The EPR spectrum is fitted by splines which, by default, correspond to cubic Bernoulli polynomial,
+#'   like e.g.
+#'   \deqn{I_{\text{EPR}}(B) = B^3 - (3/2)\,B^2 + (1/2)\,B}
+#'   where \eqn{I_{\text{EPR}}} equals to general EPR intensity and \eqn{B} is the magnetic flux density.
+#'   This may be applied to both derivative or single integrated EPR spectral forms. Also a higher/lower polynomial
+#'   degree may be applied by the \code{method} (see the argument description) and by the \code{m} optional argument
+#'   (see \code{...} additional arguments) which is a penalty order (integer). For the above-mentioned cubic
+#'   spline/polynomial \code{m = 2}. Linear polynomial corresponds
+#'   to \code{m = 1} and the highest quintic polynomial/spline is referred as \code{m = 3}:
+#'   \deqn{I_{\text{EPR}}(B) = B^5 - (5/2)\,B^4 + (5/3)\,B^3 - (1/6)\,B}
+#'   If the optional argument \code{bernoulli} is set to \code{FALSE} then "classic" definition
+#'   of a smoothing spline, where the function estimate is a piece-wise polynomial function with pieces
+#'   of degree 2m−1, is produced. Additional optional arguments from \code{\link[npreg]{ss}} like e.g. knots
+#'   definition, equivalent degrees of freedom, vector of weights...etc. can be applied as well.
 #'
 #'
-#' @return List with the following values: ...TBC...
+#' @references
+#'   \insertRef{BerryCrossValid2021}{eprscope}
+#'
+#'   \insertRef{ElezovicGeneral2016}{eprscope}
+#'
+#'   \insertRef{WeissteinBernPol2023}{eprscope}
+#'
+#'   \insertRef{HelwigMan2022}{eprscope}
+#'
+#'
+#' @inheritParams eval_gFactor_Spec
+#' @param method Character string corresponding to method to fit a smoothing spline provided by \pkg{npreg}
+#'   package (see the \code{method} argument in \code{\link[npreg]{ss}}). The following methods are available
+#'   (shortly described also
+#'   in \href{http://users.stat.umn.edu/~helwig/notes/smooth-spline-notes.html}{Nathaniel's E. Helwig Introduction}
+#'   or can be found in References) =>
+#'   \tabular{ll}{
+#'   ---------------- \tab ------------------------------------- \cr
+#'   \strong{Method} \tab \strong{Short Description} \cr
+#'   ---------------- \tab ------------------------------------- \cr
+#'   "GCV" \tab Generalized Cross-Validation \cr
+#'   "OCV" \tab Ordinary Cross-Validation \cr
+#'   "GACV" \tab Generalized Approximate Cross-Validation \cr
+#'   "ACV" \tab Approximate Cross-Validation \cr
+#'   "REML" \tab Restricted Maximum Likelihood \cr
+#'   "ML" \tab Maximum Likelihood \cr
+#'   "AIC" \tab Akaike’s Information Criterion \cr
+#'   "BIC" \tab Bayesian Information Criterion \cr
+#'   ---------------- \tab ------------------------------------- \cr
+#'   }
+#' @param output.vec Logical, whether the function output have to be vectorized, i.e. only the vector
+#'   of smoothed EPR intensity is provided. This is especially useful for the spectral (time) series EPR data,
+#'   which can be handily processed by \code{\link[dplyr]{group_by}} using the
+#'   `pipe` operators (\code{\link[magrittr]{\%>\%}}). \strong{Default}: \code{output.vec = NULL}.
+#' @param ... additional arguments passed to the function (see also \code{\link[npreg]{ss}} function).
+#'
+#'
+#' @return If \code{output.vec = TRUE} the output corresponds to vector of the smoothed EPR intensity
+#'   (either dereivative \code{lineSpecs.form = "derivative"} or integrated
+#'   \code{lineSpecs.form = "integrated"/"absorption"}) with the length of the original \code{Intensity}.
+#'   Contrarily if the \code{output.vec} is set to \code{FALSE} the following list is returned =>
+#'   \describe{
+#'   \item{df}{Original data frame with the addition column corresponding to smoothed EPR intensity
+#'   (derivative or integrated).}
+#'   \item{plot}{Plot/Graph object \emph{EPR intensity} \emph{vs.} \emph{B} with the experimental
+#'   data and its corresponding smoothed relationship performed by splines.}
+#'   \item{rss}{Weighted (if the optional parameter \code{w} is defined) residual sum of squares.}
+#'   \item{degs.freedom}{Equivalent degrees of freedom used.}
+#'   \item{fit}{List with components to characterize the spline fit.}
+#'   \item{sigma}{Estimated error standard deviation.}
+#'   \item{aic}{Akaike’s Information Criterion (if \code{method = "AIC"}).}
+#'   \item{bic}{Bayesian Information Criterion (if \code{method = "BIC"}).}
+#'   \item{log.likehood}{Likelihood logarithm if \code{method = "REML"/"ML"}.}
+#'   }
 #'
 #'
 #' @examples
@@ -106,7 +167,8 @@ smooth_EPR_Spec_by_npreg <- function(data.spectr,
       fit = smooth.epr.spec.list$fit, ## list with fit characteristics
       sigma = smooth.epr.spec.list$sigma, ## estimated error standard deviation.
       aic = smooth.epr.spec.list$aic, ## Akaike's Information Criterion (if method is AIC)
-      bic = smooth.epr.spec.list$bic ## Bayesian Information Criterion (if method is BIC)
+      bic = smooth.epr.spec.list$bic, ## Bayesian Information Criterion (if method is BIC)
+      log.likehood = smooth.epr.spec.list$logLik ## log-likelihood (if method is REML or ML)
     )
   } else{
     results <- data.spectr$smoothed
