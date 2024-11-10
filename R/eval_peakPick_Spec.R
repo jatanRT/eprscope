277417a2438607203ec77bad356e58300b878b92
janinoterko <janitt24@duck.com>
2024-11-10 17:04:11 +0100

    docu. for peak-picking updated + code cleaning

--- R/eval_peakPick_Spec.R
+++ R/eval_peakPick_Spec.R
@@ -1,5 +1,5 @@
 #'
-#' Title
+#' Peak-Picking of EPR/ENDOR Spectra
 #'
 #'
 #' @family Evaluations
@@ -10,71 +10,126 @@
 #'
 #'
 #' @inheritParams plot_EPR_Specs
-#' @param data.spectr
-#' @param x Character string, pointing to \eqn{x}-axis/column quantity header in the original \code{data.spectr}
-#'   like magnetic flux density \eqn{B}, \eqn{g}-Value or \eqn{RF} (radio frequency),
+#' @param data.spectr Data frame object/table, containing \code{x}-values and the \code{Intensity}
+#'   (see the arguments below), from which the peaks are to be found (picked).
+#' @param x Character string, pointing to \eqn{x}-axis/column quantity header in the original
+#'   \code{data.spectr} like magnetic flux density \eqn{B}, \eqn{g}-Value or \eqn{RF} (radio frequency),
 #'   \strong{default}: \code{x = "B_mT"}.
 #' @param Intensity Character string, pointing to \code{intensity column} name in the original \code{data.spectr}
 #'   if other than \code{dIepr_over_dB} name/label is used (e.g. for simulated or integrated spectra),
 #'   \strong{default}: \code{Intesity = "dIepr_over_dB"}.
 #' @param only.peak.pn Character string, setting up the selection of positive (\code{Intenstity} > 0)
 #'   and/or negative (\code{Intensity} < 0) peaks (in the case of \code{lineSpecs.form = "derivative"})
-#'   or only positive ones (in the case of \code{lineSpecs.form = "integrated"} or \code{lineSpeccs.form = "absorption"}).
-#'   \strong{Default}: \code{only.peak.pn = NULL}, corresponding to automatic selection of positive/negative
-#'   peaks depending on \code{lineSpecs.form}. In addition to \code{only.peak.pn = "positive"}
-#'   and \code{only.peak.pn = "negative"} strings, the short code like \code{only.peak.pn = "p"} (or \code{"P"})
-#'   and \code{only.peak.pn = "n"} (or \code{"N"}) can be applied as well.
-#' @param min.peak.height Numeric, setting the \code{Intensity} threshold (its absolute value) in order filter/select
-#'   only those intensity values, which are taken to find to maxima and/or minima. \strong{Default}: \code{min.peak.height = NULL},
-#'   corresponding to \eqn{20\\%} of the maximum Intensity value.
+#'   or only positive ones (in the case of \code{lineSpecs.form = "integrated"}
+#'   or \code{lineSpeccs.form = "absorption"}). \strong{Default}: \code{only.peak.pn = NULL},
+#'   corresponding to automatic selection of positive/negative peaks depending on \code{lineSpecs.form}.
+#'   In addition to \code{only.peak.pn = "positive"} and \code{only.peak.pn = "negative"} strings,
+#'   the short code like \code{only.peak.pn = "p"} (or \code{"P"}) and \code{only.peak.pn = "n"} (or \code{"N"})
+#'   can be applied as well.
+#' @param min.peak.height Numeric, setting the \code{Intensity} threshold (its absolute value)
+#'   in order filter/select only those intensity values, which are taken to find to maxima and/or minima.
+#'   \strong{Default}: \code{min.peak.height = NULL}, corresponding to \eqn{20\,\%} of the maximum Intensity value.
 #' @param min.peak.dist Numeric (integer > 0), pointing to minimum distance (in points) between the expected peaks,
-#'   which are constructed by parabola fits over the points. For such purpose, the shortened (vertex) parabola (the 2nd polynomial)
-#'   expression like \eqn{a\,(x - h)^2 + k} is applied, where \eqn{a} and \eqn{h,k} denote the concavity and the vertex, respectively.
-#'   These vertices actually correspond to peak maxima (\eqn{a < 0}) or minima (\eqn{a > 0}).
-#'   Peaks separated by less than this distance are considered as a single peak. Please, also refer to documentation
-#'   of the \code{\link[gsignal]{findpeaks}} function. The \strong{default} distance (\code{min.peak.dist = NULL}) actually equals
+#'   which are constructed by parabola fits over the points. For such purpose, the shortened (vertex)
+#'   parabola (the 2nd polynomial) expression like \eqn{a\,(x - h)^2 + k} is applied,
+#'   where \eqn{a} and \eqn{h,k} denote the concavity and the vertex, respectively. These vertices actually
+#'   correspond to peak maxima (\eqn{a < 0}) or minima (\eqn{a > 0}). Peaks separated by less than this distance
+#'   are considered as a single peak. Please, also refer to documentation of the \code{\link[gsignal]{findpeaks}}
+#'   function. The \strong{default} distance (\code{min.peak.dist = NULL}) actually equals
 #'   to one-half divided by the distance between the adjacent points, rounded to the integer: e.g.
 #'   \deqn{round(0.5\,/\,(x_2 - x_1))}
-#'   where such formula corresponds to \code{round(0.5/(data.spectr[[x]][2] - data.spectr[[x]][1]))}. This is especially useful
-#'   for rather noisy EPR spectra or spectra with high resolution. If according to \code{{ggplot2}} graphical representation
-#'   the peak-picking fails, i.e. not all peaks are properly detected, try lower values than the \strong{default} one
-#'   (such as 1 or 4 or ...etc.).
-#' @param min.peak.width Numeric (integer > 0), setting the minimum peak-width (points) to fit the vertex parabola expression
-#'   (see also the \code{min.peak.dist} argument and the \code{\link[gsignal]{findpeaks}} documentation) to find the peaks.
-#'   \strong{Default}: \code{min.peak.width = 1}.
-#' @param max.peak.width Numeric (integer > 0), pointing to maximum peak-width (points) to find the peaks. \strong{Default}:
-#'   \code{max.peak.width = Inf} (infinity).
-#' @param double.sided Logical. Should be the peaks found for both intensity sites (\code{data.spectr[[Intensity]]} > 0
-#'   as well as \code{data.spectr[[Intensity]]} < 0)? If \code{lineSpecs.form = "derivative"} then \code{double.sided = TRUE},
-#'   \strong{default}, otherwise, for the single integrated EPR spectra, it applies \code{double.sided = FALSE}.
+#'   where such formula corresponds to \code{round(0.5/(data.spectr[[x]][2] - data.spectr[[x]][1]))}.
+#'   This is especially useful for rather noisy EPR spectra or spectra with high resolution.
+#'   If according to \code{{ggplot2}} graphical representation the peak-picking fails, i.e. not all peaks
+#'   are properly detected, try lower values than the \strong{default} one (such as 1 or 4 or ...etc.).
+#' @param min.peak.width Numeric (integer > 0), setting the minimum peak-width (points) to fit the vertex
+#'   parabola expression (see also the \code{min.peak.dist} argument and the \code{\link[gsignal]{findpeaks}}
+#'   documentation) to find the peaks. \strong{Default}: \code{min.peak.width = 1}.
+#' @param max.peak.width Numeric (integer > 0), pointing to maximum peak-width (points) to find the peaks.
+#'   \strong{Default}: \code{max.peak.width = Inf} (infinity).
+#' @param double.sided Logical. Should be the peaks found for both intensity sites
+#'   (\code{data.spectr[[Intensity]]} > 0 as well as \code{data.spectr[[Intensity]]} < 0)?
+#'   If \code{lineSpecs.form = "derivative"} then \code{double.sided = TRUE}, \strong{default}, otherwise,
+#'   for the single integrated EPR spectra, it applies \code{double.sided = FALSE}.
 #' @param line.color Character string, line color to plot the EPR/ENDOR spectrum.
-#'   All \href{https://ggplot2.tidyverse.org/reference/aes_colour_fill_alpha.html}{\code{{ggplot2}} colors} are available
-#'   (see also \code{\link[ggplot2]{aes_colour_fill_alpha}}). \strong{Default}: \code{line.color = "darkviolet"}.
+#'   All \href{https://ggplot2.tidyverse.org/reference/aes_colour_fill_alpha.html}{\code{{ggplot2}} colors}
+#'   are available (see also \code{\link[ggplot2]{aes_colour_fill_alpha}}).
+#'   \strong{Default}: \code{line.color = "darkviolet"}.
 #' @param peak.color Character string, "point" color to visualize/emphasize the peaks.
 #'   Similarly, as for the \code{line.color} argument, all \code{{ggplot2}} color definitions are available.
 #'   \strong{Default}: \code{peak.color = "steelblue"}.
-#' @param peak.text.angle Numeric, setting the angle (in deg) of the peak value (projection onto the \eqn{x}-axis) annotation text,
-#'   presented near the local maximum or minimum, and measured relatively to the \eqn{x}-axis
-#'   (see also \code{\link[ggplot2]{geom_text}}). \strong{Default}: \code{peak.text.angle = 90}.
+#' @param peak.text.angle Numeric, setting the angle (in deg) of the peak value (projection onto
+#'   the \eqn{x}-axis) annotation text, presented near the local maximum or minimum, and measured relatively
+#'   to the \eqn{x}-axis (see also \code{\link[ggplot2]{geom_text}}). \strong{Default}: \code{peak.text.angle = 90}.
 #' @param peak.text.size Numeric, pointing to peak annotation text size
 #'   (in mm, see the \code{\link[ggplot2]{aes_linetype_size_shape}}). \strong{Default}: \code{peak.text.size = 3}.
 #' @param peak.point.size Numeric, size (in mm) of the peak "point" in graphical representation of the peak-picking.
 #'   Please consult the \code{\link[ggplot2]{aes_linetype_size_shape}}
 #'   \href{https://ggplot2-book.org/scales-other}{\code{{ggplot2}} aesthetic arguments}. \strong{Default}:
 #'   \code{peak.point.size = 2}.
-#' @param peak.point.shape Numeric (integer between 0 and 24), controlling the "point" symbol like square, triangle, circle,
-#'   asterisk...etc,
-#'   refer to e.g. \href{https://ggplot2.tidyverse.org/articles/ggplot2-specs.html}{\code{{ggplot2}} aesthetic specifications}.
-#'   \strong{Default}: \code{peak.point.shape = 16} (filled circle).
-#' @param peak.text.overlap Logical, ...TBC.
-#' @param ... additional arguments specified, please refer to the \code{\link{plot_EPR_Specs}} function, in order to customize
-#'   the graphical output.
+#' @param peak.point.shape Numeric (integer between 0 and 24), controlling the "point" symbol like square,
+#'   triangle, circle, asterisk...etc,
+#'   refer to e.g. \href{https://ggplot2.tidyverse.org/articles/ggplot2-specs.html}{\code{{ggplot2}}
+#'   aesthetic specifications}. \strong{Default}: \code{peak.point.shape = 16} (filled circle).
+#' @param peak.text.overlap Logical, if \code{TRUE}, the overlapping peak text annotation to a previous one,
+#'   will be not displayed, for clarity (see also \code{\link[ggplot2]{geom_label}}).
+#'   If \code{peak.text.overlap = FALSE} (\strong{default}), all found peaks are presented with their
+#'   corresponding values (text annotations).
+#' @param ... additional arguments specified, please refer to the \code{\link{plot_EPR_Specs}} function,
+#'   in order to customize the graphical output.
 #'
 #'
 #' @return
+#'   List, consisting of following components, will be returned:
+#'   \describe{
+#'   \item{df}{Data frame, corresponding to the original \code{data.spectr}, with all peaks (\code{Intensity}
+#'   \emph{vs} \code{x}) found by \code{\link[gsignal]{findpeaks}} algorithm and the arguments defined above.}
+#'   \item{plot}{Graphical representation of found peaks in the EPR/ENDOR spectrum.}
+#'   }
 #'
 #'
 #' @examples
+#' ## loading TMPD built-in example file:
+#' tmpd.data.file.path <-
+#'   load_data_example(file = "TMPD_specelchem_accu_b.asc")
+#' ## reading data:
+#' tmpd.data.file <-
+#'   readEPR_Exp_Specs(path_to_ASC = tmpd.data.file.path,
+#'                     col.names = c("B_G",
+#'                                   "dIepr_over_dB"),
+#'                     qValue = 3500,
+#'                     norm.vec.add = 20,
+#'                     origin = "winepr")
+#' #
+#' ## peak-picking of only positive (Intensitity > 0)
+#' ## peaks in derivative EPR spectrum of TMPD:
+#' tmpd.peak.pick.01 <-
+#'   eval_peakPick_Spec(data.spectr = tmpd.data.file,
+#'                      only.peak.pn = "p",
+#'                      min.peak.dist = 1)
+#' #
+#' ## peak-picking visualization:
+#' tmpd.peak.pick.01$plot
+#' #
+#' ## found peaks data frame preview
+#' head(tmpd.peak.pick.01$df)
+#' #
+#' ## peak-picking in 'G' (not in default 'mT')
+#' ## of both positive as well as negative intensities
+#' ## with the intensity threshold 10% of the maximum,
+#' ## peaks annotation text angle 60 deg:
+#' tmpd.peak.pick.02 <-
+#'   eval_peakPick_Spec(
+#'     data.spectr = tmpd.data.file,
+#'     x = "B_G",
+#'     x.unit = "G",
+#'     min.peak.hight =
+#'       0.1 * max(tmpd.data.file$dIepr_over_dB),
+#'     peak.text.angle = 60
+#'   )
+#' #
+#' ## graph/EPR spectrum peaks preview:
+#' tmpd.peak.pick.02$plot
 #'
 #'
 #' @export
@@ -126,15 +181,18 @@ eval_peakPick_Spec <- function(data.spectr,
       double.sided <- double.sided %>% `if`(isFALSE(double.sided),TRUE,.)
       message(" For the derivative line form both positive and negative values\n
             are taken. Therefore, the `double.sided` argument \n
-            was automamtically set to TRUE ")
+            was automamtically set to TRUE. ")
     } else {
       double.sided <- double.sided
     }
   }
   if (grepl("integ|Integ|absorpt|Absorpt",lineSpecs.form)){
     if (isTRUE(double.sided)){
-      stop(" For the integrated/absorption line form only positive values\n
-           are taken. To find the peaks => put `double.sided = FALSE` !! ")
+      double.sided <- double.sided %>% `if`(isTRUE(double.sided),FALSE,.)
+      message(" For the integrated/absorption line form only positive values\n
+              are taken. Therefore, the `double.sided` argument \n
+              was automamtically set to FALSE. "
+      )
     } else {
       double.sided <- double.sided
     }
