6f5ab3f6f4777dc909c57ae84c9078543909f520
tarjb <jantar40@protonmail.com>
2026-01-22 11:32:48 +0100

    reading a/As from QCH output files updated
    fixed bug to read ORCA A/as + documentation + test added + removed the `abs` for the final A/as output table (+- signs remain) + code cleaning

--- R/rearrange_aAiso.R
+++ R/rearrange_aAiso.R
@@ -7,8 +7,8 @@
 #'
 #' @description
 #'   Providing table, based on Gaussian/ORCA/...etc. output text files in order to summarize
-#'   the mean \eqn{A_{iso}}/\eqn{a_{iso}} values of groups with equivalent nuclei, according to proposed
-#'   molecular structure/symmetry.
+#'   the mean \eqn{A_{iso}}/\eqn{a_{iso}} values (including the sign) of groups with equivalent nuclei,
+#'   according to proposed molecular structure/symmetry.
 #'
 #'
 #'
@@ -16,21 +16,21 @@
 #'   The \eqn{A_{iso}}/\eqn{a_{iso}} values are computed for each atom/nucleus
 #'   (with its corresponding \code{atomic number within the structure} as well as with the characteristic
 #'   \code{isotopic number/value}), such an entire table can be copied e.g. from \strong{Gaussian} output
-#'   (after \code{'Isotropic Fermi Contact Couplings'} line) or can be constructed from \strong{ORCA} (or any other) output,
-#'   example for such a file structure (from \strong{Gaussian}):
+#'   (after \code{'Isotropic Fermi Contact Couplings'} line) or can be constructed from \strong{ORCA} (or any other) output.
+#'   Example for a required file/table structure (e.g. for \strong{Gaussian}):
 #'   \tabular{rcccc}{
 #'    \strong{No_atom} \tab \strong{Atom_Nucleus} \tab \strong{MegaHertz} \tab \strong{Gauss} \tab \strong{1e-4_cm-1} \cr
 #'    1 \tab N(14) \tab 0.00643 \tab 0.00229 \tab 0.00214 \cr
 #'    17 \tab N(14) \tab 13.99707 \tab 4.9945 \tab 4.66892 \cr
 #'    28 \tab H(1) \tab 16.34971 \tab 5.83398 \tab 5.45368 \cr
 #'   }
-#'   The input table/data frame, like the previous one, must include following columns: atomic/nucleus number, atom/nucleus notation,
+#'   The input table/data frame, like the previous one, must include following columns: atomic/nucleus number, atom/nucleus label,
 #'   hyperfine coupling constant in \code{MHz} and finally hyperfine splitting constant in \code{G}. These columns/variables
 #'   are essential for the evaluation.
 #'
 #'
 #'
-#' @param path_to_ASC Character string, pointing to path of ASCII file (\code{txt},\code{csv}...etc,
+#' @param path_to_ASC Character string, pointing to path of ASCII file (\code{txt},\code{csv},...etc,
 #' it may be also provided by the \code{\link[base]{file.path}} function). The file must include characteristic
 #'   \eqn{A_{iso}} or \eqn{a_{iso}} values.
 #' @param col.names Character string vector, containing names of all columns from QCH computational output,
@@ -41,8 +41,8 @@
 #'   correspond to indices of proposed equivalent nuclei in the ASCII text file.
 #'
 #'
-#' @return Data frame/Table of \eqn{A_{iso}}/\eqn{a_{iso}} mean values corresponding to groups
-#'   of equivalent nuclei within the structure/symmetry.
+#' @return Data frame/Table of \eqn{A_{iso}}/\eqn{a_{iso}} mean values (including the \eqn{\pm} signs) corresponding
+#'   to groups of equivalent nuclei within the structure/symmetry.
 #'
 #'
 #' @examples
@@ -67,37 +67,57 @@ rearrange_aAiso_QCHcomp <- function(path_to_ASC,
   mT <- NULL
   #
   ## Conditions/Extraction for column names:
-  ## use stringr::str_subset(...) or X[grepl(...)] or stringr::str_extract or grep(...,value = T)
-  A.str <- str_subset(col.names,
-                               regex("mhz|megahertz",
-                                     ignore_case = T))
-  a.str <- str_subset(col.names,
-                               regex("gauss|G|Gauss"))
-  atomic.num.str <- str_subset(col.names,
-                                        regex("No|Num|num|no|no_|num_|NUM|Num_|NUM_|NO|NO_|No_"))
-  nucl.str <- str_subset(col.names,
-                                  regex("nuc|Nuc_atom|nucleus_|NUC|NUC_|ATOM|atom",
-                                        ignore_case = T))
+  ## use stringr::str_subset(...) or X[grepl(...)]
+  ## or stringr::str_extract or grep(...,value = T)
+  A.str <- str_subset(
+    col.names,
+    regex("mhz|megahertz",ignore_case = T)
+  )
+  a.str <- str_subset(col.names,regex("gauss|G|Gauss"))
+  atomic.num.str <- str_subset(
+    col.names,
+    regex("No|Num|num|no|no_|num_|NUM|Num_|NUM_|NO|NO_|No_")
+  )
+  nucl.str <- str_subset(
+    col.names,
+    regex(
+      "nuc|Nuc_atom|nucleus_|NUC|NUC_|ATOM|atom",
+      ignore_case = T
+    )
+  )
   #
   ## Read the data:
-  data.Aa.comput <- fread(path_to_ASC,
-                                      sep = "auto", header = F, skip = 1,
-                                      col.names = col.names
+  data.Aa.comput <- fread(
+    path_to_ASC,
+    sep = "auto",
+    header = F,
+    skip = 1,
+    col.names = col.names
   ) %>%
     dplyr::mutate(
       MegaHertz = abs(round(.data[[A.str]], digits = 3)),
       Gauss = abs(round(.data[[a.str]], digits = 2))
     ) %>%
-    dplyr::select(dplyr::all_of(c(atomic.num.str,
-                                  nucl.str,
-                                  "MegaHertz",
-                                  "Gauss")))
+    dplyr::select(
+      dplyr::all_of(
+        c(
+          atomic.num.str,
+          nucl.str,
+          "MegaHertz",
+          "Gauss"
+        )
+      )
+    )
   #
   ## Own function to rearrange A/a according to `nuclei.list.slct`
   ## Build up new rearranged data frame for Nuclei A/a:
-  data.slct.nucs.group <- data.frame(
-    No = integer(), Nucleus = character(),
-    MegaHertz = numeric(), Gauss = numeric(), NuclearGroup = character()
+  data.slct.nucs.group <-
+    data.frame(
+      No = integer(),
+      Nucleus = character(),
+      MegaHertz = numeric(),
+      Gauss = numeric(),
+      NuclearGroup = character()
   )
   #
   ## cycle for each `nuclei.list.slct` component
@@ -105,40 +125,43 @@ rearrange_aAiso_QCHcomp <- function(path_to_ASC,
     #
     ## Filter atomic numbers from each part of the list:
     sal <- data.Aa.comput %>%
-      dplyr::filter(.data[[atomic.num.str]] %in% nuclei.list.slct[[k]])
-    #
+      dplyr::filter(
+        .data[[atomic.num.str]] %in% nuclei.list.slct[[k]]
+      )
     ## How many atoms/nuclei are included in each list part:
     how.many.nucs <- length(nuclei.list.slct[[k]])
-    #
     ## Extract atomic/nuclear label:
     mark.nucs <- str_extract(sal[[nucl.str]][1], "[[:alpha:]]+")
-    #
     ## Extract isotope number:
     nucleo.nucs <- str_extract(sal[[nucl.str]][1], "[[:digit:]]+")
-    #
     ## Extract atomic/nuclear numbers and collapse it into one string:
     num.nucs.str <- paste(sal[[atomic.num.str]], collapse = ",")
-    #
     ## Now combine all four variables by paste:
-    group.nucs <- paste0(how.many.nucs, " x ", nucleo.nucs, mark.nucs, " (", num.nucs.str, ")")
+    group.nucs <- paste0(
+      how.many.nucs,
+      " x ",
+      nucleo.nucs,
+      mark.nucs,
+      " (", num.nucs.str, ")"
+    )
     #
-    ## Replicate the previous string variable corresponding to length of a list component/part:
+    ## Replicate the previous string variable corresponding
+    ## to length of a list component/part:
     NuclearGroup <- rep(group.nucs, length(nuclei.list.slct[[k]]))
-    #
     ## Build up data frame row by row:
     gal <- data.frame(sal, NuclearGroup) ## at first combine by columns
     data.slct.nucs.group <- rbind(data.slct.nucs.group, gal)
   }
-  ## Group by and summarize (find the mean value of each list component) according
-  ## to `nuclei.list.slct` (symmetry) and previous `for loop`:
-  #
+  ## Group by and summarize (find the mean value of each list
+  ## component) according to `nuclei.list.slct` (symmetry)
+  ## and previous `for loop`:
   data.slct.nucs.group <- data.slct.nucs.group %>%
     dplyr::mutate(mT = .data$Gauss / 10) %>%
     dplyr::select(-dplyr::all_of(c("No","Nucleus","Gauss"))) %>%
     dplyr::group_by(.data$NuclearGroup) %>%
     dplyr::summarize(
-      Aiso_MHz_QCH = abs(round(mean(.data$MegaHertz), digits = 2)),
-      aiso_mT_QCH = abs(round(mean(.data$mT), digits = 2))
+      Aiso_MHz = round(mean(.data$MegaHertz), digits = 2),
+      aiso_mT = round(mean(.data$mT), digits = 2)
     )
   #
   ## Entire output table:
@@ -157,14 +180,14 @@ rearrange_aAiso_QCHcomp <- function(path_to_ASC,
 #'
 #' @description
 #'   Providing table, specifically from \code{Gaussian} or \code{ORCA} output text files to summarize
-#'   the \eqn{A_{iso}}/\eqn{a_{iso}} mean values of groups with equivalent nuclei, according
-#'   to proposed molecular structure/symmetry (see also the \code{\link{rearrange_aAiso_QCHcomp}}).
+#'   the \eqn{A_{iso}}/\eqn{a_{iso}} mean values (including the sign) of groups with equivalent nuclei,
+#'   according to proposed molecular structure/symmetry (see also the \code{\link{rearrange_aAiso_QCHcomp}}).
 #'
 #'
 #' @inheritParams rearrange_aAiso_QCHcomp
 #' @param path_to_QCHoutput Character string, corresponding to path of \code{Gaussian} or \code{ORCA} output text files.
 #' @param N.nuclei Numeric value that equals to number of atoms/nuclei within the calculated structure.
-#' @param origin Character string, pointing to origin of DFT EPR calculation parameters <=> which
+#' @param origin Character string, pointing to origin of (DFT) EPR calculation parameters <=> which
 #'   software package was used. Only two values are available => \code{"gaussian"} (\strong{default})
 #'   or \code{"orca"}.
 #' @param output.text.origin Logical, whether to write a text file containing the extracted
@@ -175,8 +198,8 @@ rearrange_aAiso_QCHcomp <- function(path_to_ASC,
 #'   by the \code{path_to_QCHoutput}. See also the previous argument.
 #'
 #'
-#' @return Data frame/Table of \eqn{A_{iso}}/\eqn{a_{iso}} mean values, corresponding to groups
-#'   of proposed equivalent nuclei within the structure/symmetry constructed directly
+#' @return Data frame/Table of \eqn{A_{iso}}/\eqn{a_{iso}} mean values (including the \eqn{\pm} signs),
+#'   corresponding to groups of proposed equivalent nuclei within the structure/symmetry constructed directly
 #'   from \emph{Gaussian} or \emph{ORCA} output text files.
 #'
 #'
@@ -198,7 +221,10 @@ rearrange_aAiso_QCHcomp <- function(path_to_ASC,
 #'      )
 #' #
 #' ## preview
-#' symmetry.As.df
+#' knitr::kable(
+#'   symmetry.As.df,
+#'   caption = "DFT computed hyperfine couplings(Aiso)/splittings(aiso) for the TMPD radical cation."
+#' )
 #'
 #'
 #' @export
@@ -222,10 +248,11 @@ rearrange_aAiso_QCHorgau <- function(path_to_QCHoutput,
   ## ============================ READING `qchfiles` ==============================
   #
   ## Processing of output file depending on the origin
-  if (origin == "gaussian" || origin == "Gaussian" || origin == "GAUSSIAN") {
+  if (any(grepl("Gauss|GAUSS|gauss",origin))) {
     #
     ## number of atoms + 1
-    ## because it also reads the header therefore, there must be an additional line
+    ## because it also reads the header therefore,
+    ## there must be an additional line
     No_nuclei_atoms_mod <- No_nuclei_atoms + 1
     #
     ## indicator (String) to select specific lines (indices) from `qchfile`
@@ -236,13 +263,15 @@ rearrange_aAiso_QCHorgau <- function(path_to_QCHoutput,
     ## however there are two `start.reading.line`
     ## character strings in Gaussian EPR Output
     ## therefore, select only the first one
-    qchfile.select.A <- qchfile[start.reading.line[1] + 1:No_nuclei_atoms_mod]
+    qchfile.select.A <-
+      qchfile[start.reading.line[1] + 1:No_nuclei_atoms_mod]
     #
     ## output original gaussian data file in-between:-) =>
     if (isTRUE(output.text.origin)) {
-      fileConn <- ifelse(is.null(output.text.path),
-                         stop(" Please define `.txt` file path to be saved ! "),
-                         file(output.text.path)
+      fileConn <- ifelse(
+        is.null(output.text.path),
+        stop(" Please define `.txt` file path to be saved ! "),
+        file(output.text.path)
       )
       writeLines(qchfile.select.A, fileConn)
       close(fileConn)
@@ -253,8 +282,12 @@ rearrange_aAiso_QCHorgau <- function(path_to_QCHoutput,
     qchfile.select.A <- str_split(qchfile.select.A, pattern = "\\s+")
     #
     ## create a data frame, the first row line is the header,
-    table.select.A <- data.frame("No" = character(), "Nucleus" = character(),
-                                 "Megahertz" = character(),"Gauss" = character())
+    table.select.A <- data.frame(
+      "No" = character(),
+      "Nucleus" = character(),
+      "Megahertz" = character(),
+      "Gauss" = character()
+    )
     ## therefore start form [[2]] `second row/line`
     for (i in 2:No_nuclei_atoms_mod) {
       table.select.B <- data.frame(
@@ -270,12 +303,13 @@ rearrange_aAiso_QCHorgau <- function(path_to_QCHoutput,
     ## converting columns into character and numeric (double) format
     table.select.A$No <- as.numeric(table.select.A$No)
     table.select.A$Nucleus <- as.character(table.select.A$Nucleus)
+    #
     for (j in 3:length(colnames(table.select.A))) {
       table.select.A[, j] <- as.numeric(table.select.A[, j])
     }
     #
   }
-  if (origin == "orca" || origin == "Orca" || origin == "ORCA") {
+  if (any(grepl("ORCA|orca|Orca",origin))) {
     ## indicator (String) to select specific lines (indices) from `qchfile`
     ## in the case of `ORCA` this indicator tells that this is the last part
     ## of `qchfile` with EPR parameters (`ORCA`) has different output file
@@ -288,15 +322,16 @@ rearrange_aAiso_QCHorgau <- function(path_to_QCHoutput,
     #
     ## output original orca data file in-between:-) =>
     if (isTRUE(output.text.origin)) {
-      fileConn <- ifelse(is.null(output.text.path),
-                         stop(" Please define `.txt` file path to be saved ! "),
-                         file(output.text.path)
+      fileConn <- ifelse(
+        is.null(output.text.path),
+        stop(" Please define `.txt` file path to be saved ! "),
+        file(output.text.path)
       )
       writeLines(qchfile.select, fileConn)
       close(fileConn)
     }
-    ## Reading the lines with relevant information (Atoms + A HF couplings)
-    ## Atoms/Nuclei
+    ## Reading the lines with relevant information
+    ## (Atoms + A HF couplings) Atoms/Nuclei
     nucleus.indicator <- "Nucleus"
     nuclei.lines <- grep(nucleus.indicator, qchfile.select)
     ## reading only for selected number of atoms
@@ -305,13 +340,15 @@ rearrange_aAiso_QCHorgau <- function(path_to_QCHoutput,
       function(x) qchfile.select[nuclei.lines][[x]]
     )
     ## splitting the strings
-    nuclei.qchfile.select <- str_split(nuclei.qchfile.select, pattern = "\\s+")
-    ## separate atomic/nucleus number (`n`) and atomic/nucleus label (`L`) + isotope (`I`)
+    nuclei.qchfile.select <-
+      str_split(nuclei.qchfile.select, pattern = "\\s+")
+    ## separate atomic/nucleus number (`n`)
+    ## and atomic/nucleus label (`L`) + isotope (`I`)
     nuclei.qchfile.select.n <- sapply(
       seq(nuclei.qchfile.select),
       function(n) {
         str_extract(
-          nuclei.qchfile.select[[n]][2],
+          nuclei.qchfile.select[[n]][3],
           "[[:digit:]]+"
         )
       }
@@ -320,14 +357,14 @@ rearrange_aAiso_QCHorgau <- function(path_to_QCHoutput,
       seq(nuclei.qchfile.select),
       function(L) {
         str_extract(
-          nuclei.qchfile.select[[L]][2],
+          nuclei.qchfile.select[[L]][3],
           "[[:alpha:]]+"
         )
       }
     )
     nuclei.qchfile.select.I <- sapply(
       seq(nuclei.qchfile.select),
-      function(I) nuclei.qchfile.select[[I]][5]
+      function(I) nuclei.qchfile.select[[I]][6]
     )
     ## combine last two vectors into one like `H(1)` in order to be consistent
     ## with the Gaussian output
@@ -342,19 +379,27 @@ rearrange_aAiso_QCHorgau <- function(path_to_QCHoutput,
     ## therefore one should escape the backslashes
     A.lines <- grep(A.indicator, qchfile.select)
     ## reading only for selected number of atoms
-    A.qchfile.select <- lapply(seq(No_nuclei_atoms),
-                               function(y) qchfile.select[A.lines][[y]])
+    A.qchfile.select <- lapply(
+      seq(No_nuclei_atoms),
+      function(y) qchfile.select[A.lines][[y]]
+    )
     ## splitting the strings
     A.qchfile.select <- str_split(A.qchfile.select, pattern = "\\s+")
     ## select the A iso values by [[i]][6] for i-th list/line element
     A.qchfile.select.iso <- sapply(
       seq(A.qchfile.select),
-      function(i) A.qchfile.select[[i]][6]
+      function(i) {
+        str_extract(
+          A.qchfile.select[[i]][6],
+          "[-+]?[0-9]*\\.[0-9]+" ## also must select the sign
+        )
+      }
     )
     #
     ## Generate the data frame,
     ## first of all define numeric columns
-    A.qchfile.select.iso.mhz <- as.numeric(A.qchfile.select.iso)
+    A.qchfile.select.iso.mhz <-
+      round(as.numeric(A.qchfile.select.iso),digits = 3)
     A.qchfile.select.iso.gauss <-
       convert_A_MHz_2a(as.numeric(A.qchfile.select.iso)) * 10
     #
@@ -374,8 +419,11 @@ rearrange_aAiso_QCHorgau <- function(path_to_QCHoutput,
   ## Own function to rearrange A/a according to `nuclei.list.slct`
   ## Build up new rearranged data frame for Nuclei A/a:
   data.slct.nucs.group <- data.frame(
-    "No" = integer(), "Nucleus" = character(),
-    "Megahertz" = numeric(), "Gauss" = numeric(), "NuclearGroup" = character()
+    "No" = integer(),
+    "Nucleus" = character(),
+    "Megahertz" = numeric(),
+    "Gauss" = numeric(),
+    "NuclearGroup" = character()
   )
   #
   ## cycle for each `nuclei.list.slct` component
@@ -384,39 +432,41 @@ rearrange_aAiso_QCHorgau <- function(path_to_QCHoutput,
     ## Filter atomic numbers from each part of the list:
     sal <- table.select.A %>%
       dplyr::filter(.data$No %in% nuclei.list.slct[[k]])
-    #
     ## How many atoms/nuclei are included in each list part:
     how.many.nucs <- length(nuclei.list.slct[[k]])
-    #
     ## Extract atomic/nuclear label:
     mark.nucs <- str_extract(sal$Nucleus[1], "[[:alpha:]]+")
-    #
     ## Extract isotope number:
     nucleo.nucs <- str_extract(sal$Nucleus[1], "[[:digit:]]+")
-    #
-    ## Extract atomic/nuclear numbers and collapse it into one string:
+    ## Extract atomic/nuclear numbers
+    ## and collapse it into one string:
     num.nucs.str <- paste(sal$No, collapse = ",")
-    #
     ## Now combine all four variables by paste:
-    group.nucs <- paste0(how.many.nucs, " x ", nucleo.nucs, mark.nucs, " (", num.nucs.str, ")")
-    #
-    ## Replicate the previous string variable corresponding to length of a list component/part:
+    group.nucs <- paste0(
+      how.many.nucs,
+      " x ",
+      nucleo.nucs,
+      mark.nucs,
+      " (", num.nucs.str, ")"
+    )
+    ## Replicate the previous string variable corresponding
+    ## to length of a list component/part:
     NuclearGroup <- rep(group.nucs, length(nuclei.list.slct[[k]]))
-    #
     ## Build up data frame row by row:
     gal <- data.frame(sal, NuclearGroup) ## at first combine by columns
     data.slct.nucs.group <- rbind(data.slct.nucs.group, gal)
   }
-  ## Group by and summarize (find the mean value of each list component) according
-  ## to `nuclei.list.slct` (symmetry) and previous `for loop`:
+  ## Group by and summarize (find the mean value
+  ## of each list component) according to `nuclei.list.slct`
+  ## (symmetry) and previous `for loop`:
   #
   data.slct.nucs.group <- data.slct.nucs.group %>%
     dplyr::mutate(mT = .data$Gauss / 10) %>%
     dplyr::select(-dplyr::all_of(c("No","Nucleus","Gauss"))) %>%
     dplyr::group_by(.data$NuclearGroup) %>%
     dplyr::summarize(
-      Aiso_MHz_QCH = abs(round(mean(.data$Megahertz), digits = 2)),
-      aiso_mT_QCH = abs(round(mean(.data$mT), digits = 2))
+      Aiso_MHz = round(mean(.data$Megahertz), digits = 2),
+      aiso_mT = round(mean(.data$mT), digits = 2)
     )
   #
   ## Finally rearranged data frame
---
+++ tests/testthat/test-rearrange_aAiso_QCHorgau.R
@@ -0,0 +1,50 @@
+test_that("Hyperfine Coupling(A)/Splitting(a) Constants, for the TMPD Radical Cation, Matches.", {
+  #
+  ## load the data
+  gauss.file.path <-
+    load_data_example(file = "TMPDAradCatEPRa.inp.log.zip")
+  gauss.file <- unzip(gauss.file.path)
+  #
+  ## list of selected nuclei (numbers)
+  nucs.slct <-
+    list(c(7, 8), ## 2 x 14N
+         c(13, 14, 15, 16), ## 4 x 1H (aromatic)
+         c(17, 18, 19, 20,
+           21, 22, 23, 24,
+           25, 26, 27, 28) ## 12 x 1H (methyl groups)
+    )
+  #
+  ## a/A extraction from file + rearrangement
+  symmetry.aAs.df <-
+    rearrange_aAiso_QCHorgau(
+      gauss.file,
+      N.nuclei = 28,
+      nuclei.list.slct = nucs.slct
+    )
+  #
+  ## remove the `log` (not required anymore)
+  file.remove("TMPDAradCatEPRa.inp.log")
+  #
+  ## check the selected nuclei list from the output `df`
+  ## with that in the original list
+  expect_equal(
+    (nchar(symmetry.aAs.df$NuclearGroup[1]) - 21)/2, ## should be 12
+    length(nucs.slct[[3]]) ## should be 12
+  )
+  expect_equal(
+    (nchar(symmetry.aAs.df$NuclearGroup[3]) - 12)/2, ## should be 4
+    length(nucs.slct[[2]]) ## shold be 4
+  )
+  #
+  ## check the columns of the `symmetry.aAs.df`
+  expect_equal(
+    symmetry.aAs.df$Aiso_MHz,
+    c(20.25,17.52,-5.24)
+  )
+  #
+  expect_equal(
+    symmetry.aAs.df$aiso_mT,
+    c(0.72,0.63,-0.19)
+  )
+  #
+})
