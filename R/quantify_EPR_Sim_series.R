52141e686b863408532efb8eaf057b6d5e45c126
janinoterko <janitt24@duck.com>
2026-08-29 22:03:02 +0200

    small updates + new version

--- R/quantify_EPR_Sim_series.R
+++ R/quantify_EPR_Sim_series.R
@@ -149,7 +149,7 @@
 #'      name.pattern.sim = "DHMB0_1st_04_SimA",
 #'      optim.params.init = c(0,0.8),
 #'      output.area.stat = TRUE)
-#' ## progress bar - activated
+#' ## progress bar - automatically activated
 #' #
 #' ## similar example with two components
 #' ## (simulated spectra) and tidy data frame
@@ -166,7 +166,9 @@
 #'      output.area.stat = FALSE,
 #'      eval.optim.progress = TRUE
 #'      )
-#' ## progress bar - deactivated
+#' ## progress bar - automatically deactivated,
+#' ## because `eval.optim.progress = TRUE`, otherwise
+#' ## both messages 'collide' in the output
 #' #
 #' ## to implement the `.csv` outputs
 #' ## from `plot_eval_ExpSim_app()`, the following
